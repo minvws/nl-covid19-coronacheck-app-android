b8e0c077566220c4a3438af4c06316c1c2448168
Bart Nijland <bartnijland91@gmail.com>
2021-02-03 16:09:35 +0100

    Re-added removed test

---
+++ shared/src/test/java/nl/rijksoverheid/ctr/shared/api/FakeTestApiClient.kt
@@ -0,0 +1,48 @@
+/*
+  *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+  *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+  *  
+  *   SPDX-License-Identifier: EUPL-1.2
+  *
+  */
+
+package nl.rijksoverheid.ctr.shared.api
+
+import nl.rijksoverheid.ctr.shared.models.*
+import okhttp3.ResponseBody
+import retrofit2.Response
+
+abstract class FakeTestApiClient : TestApiClient {
+
+    override suspend fun getAgent(id: String): RemoteAgent {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getEvent(id: String): RemoteEvent {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getHolderConfig(): Config {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getIssuers(): Issuers {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getNonce(): RemoteNonce {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTestIsm(
+        accessToken: String,
+        sToken: String,
+        icm: String
+    ): Response<ResponseBody> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getVerifierConfig(): Config {
+        TODO("Not yet implemented")
+    }
+}
