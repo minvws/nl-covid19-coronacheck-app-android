22139705f7fa4225ff045916a49b1f8690a14a39
Bart Nijland <bartnijland91@gmail.com>
2021-02-05 16:41:18 +0100

    Trigger CD

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/MainActivity.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
