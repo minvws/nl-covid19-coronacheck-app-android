4339b81c7e6766585507472d5b7fd0c4e571746d
Bart Nijland <bartnijland91@gmail.com>
2021-02-05 16:57:07 +0100

    Fix cd

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/MainActivity.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
