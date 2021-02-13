0d439b4ac6b808fcff74a451457a72e80e76529f
Bart Nijland <bartnijland91@gmail.com>
2021-02-13 15:17:00 +0100

    Bump ctcl to latest version

--- fetch_ctcl.sh
+++ fetch_ctcl.sh
@@ -1,6 +1,5 @@
-git clone git@github.com:minvws/nl-covid19-coronatester-ctcl-core-private.git tmp-ctcl
+git clone git@github.com:minvws/nl-covid19-coronatester-ctcl-core.git tmp-ctcl
 cd tmp-ctcl
-git checkout app-integration
 gomobile bind -target android -o ctcl.aar github.com/minvws/nl-covid19-coronatester-ctcl-core/clmobile
 cd ../
 cp tmp-ctcl/ctcl.aar shared/libs 
