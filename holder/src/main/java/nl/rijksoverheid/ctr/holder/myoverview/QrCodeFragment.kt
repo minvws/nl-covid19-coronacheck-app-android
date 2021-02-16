7cca24622bf005b9651f32c3ab232c1d268fdbd8
Bart Nijland <bartnijland91@gmail.com>
2021-02-16 08:29:27 +0100

    Create QrCodeFragment

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/myoverview/QrCodeFragment.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/myoverview/QrCodeFragment.kt
@@ -1,6 +1,11 @@
 package nl.rijksoverheid.ctr.holder.myoverview
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import nl.rijksoverheid.ctr.holder.databinding.FragmentQrCodeBinding
 
 /*
  *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
@@ -10,5 +15,17 @@ import androidx.fragment.app.Fragment
  *
  */
 class QrCodeFragment : Fragment() {
+
+    private lateinit var binding: FragmentQrCodeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentQrCodeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
 
 }
