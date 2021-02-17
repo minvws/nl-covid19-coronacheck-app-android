c0bc223cc84f99bd95f7585821e3dc061e79a0f3
Bart Nijland <bartnijland91@gmail.com>
2021-02-17 14:24:08 +0100

    Add AboutThisAppFragment

---
+++ verifier/src/main/java/nl/rijksoverheid/ctr/verifier/about/AboutThisAppFragment.kt
@@ -0,0 +1,26 @@
+package nl.rijksoverheid.ctr.verifier.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import nl.rijksoverheid.ctr.verifier.databinding.FragmentAboutThisAppBinding
+
+/*
+ *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *   SPDX-License-Identifier: EUPL-1.2
+ *
+ */
+class AboutThisAppFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentAboutThisAppBinding.inflate(inflater, container, false).root
+    }
+}
