806e4458f6c8476eec493a6f12a43af45ebf4a2d
Bart Nijland <bartnijland91@gmail.com>
2021-02-18 13:18:30 +0100

    Added launch screens

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/MainActivity.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
