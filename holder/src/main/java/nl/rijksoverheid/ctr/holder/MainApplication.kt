43b3e47b8cec6b4219875c698732407c578b188c
Bart Nijland <bartnijland91@gmail.com>
2021-02-23 08:42:34 +0100

    Make base api url configurable in ApiModule

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/MainApplication.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/MainApplication.kt
@@ -35,7 +35,7 @@ class MainApplication : SharedApplication(), CoronaCheckApp {
                 sharedModule,
                 appConfigModule("holder", BuildConfig.VERSION_CODE),
                 introductionModule,
-                apiModule
+                apiModule(nl.rijksoverheid.ctr.api.BuildConfig.BASE_API_URL)
             )
         }
     }
