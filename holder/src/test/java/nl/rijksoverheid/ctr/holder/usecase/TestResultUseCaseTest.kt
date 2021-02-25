2b5fe1c52dc0332e41030baf30f664664cdb17df
Bart Nijland <bartnijland91@gmail.com>
2021-02-25 13:49:49 +0100

    Finished test cases for TestResultUseCaseTest

--- holder/src/test/java/nl/rijksoverheid/ctr/holder/usecase/TestResultUseCaseTest.kt
+++ holder/src/test/java/nl/rijksoverheid/ctr/holder/usecase/TestResultUseCaseTest.kt
@@ -1,10 +1,18 @@
 package nl.rijksoverheid.ctr.holder.usecase
 
 import kotlinx.coroutines.runBlocking
+import nl.rijksoverheid.ctr.api.models.RemoteTestProviders
+import nl.rijksoverheid.ctr.api.models.RemoteTestResult
+import nl.rijksoverheid.ctr.api.models.SignedResponseWithModel
 import nl.rijksoverheid.ctr.holder.*
-import org.junit.Assert
+import nl.rijksoverheid.ctr.holder.repositories.TestProviderRepository
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+import java.time.OffsetDateTime
 
 /*
  *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
@@ -26,6 +34,191 @@ class TestResultUseCaseTest {
         )
         val result = usecase.testResult(uniqueCode = "dummy")
         assertTrue(result is TestResult.InvalidToken)
+    }
+
+    @Test
+    fun `testResult returns InvalidToken if no provider matches`() = runBlocking {
+        val usecase = TestResultUseCase(
+            testProviderUseCase = fakeTestProviderUseCase(),
+            testProviderRepository = fakeTestProviderRepository(),
+            coronaCheckRepository = fakeCoronaCheckRepository(),
+            commitmentMessageUseCase = fakeCommitmentMessageUsecase(),
+            secretKeyUseCase = fakeSecretKeyUseCase()
+        )
+        val result = usecase.testResult(uniqueCode = "provider-code")
+        assertTrue(result is TestResult.InvalidToken)
+    }
+
+    @Test
+    fun `testResult returns Complete if RemoteTestResult status is Complete`() = runBlocking {
+        val providerIdentifier = "provider"
+        val usecase = TestResultUseCase(
+            testProviderUseCase = fakeTestProviderUseCase(
+                provider = getRemoteTestProvider(
+                    identifier = providerIdentifier
+                )
+            ),
+            testProviderRepository = fakeTestProviderRepository(
+                model =
+                getRemoteTestResult(status = RemoteTestResult.Status.COMPLETE)
+            ),
+            coronaCheckRepository = fakeCoronaCheckRepository(),
+            commitmentMessageUseCase = fakeCommitmentMessageUsecase(),
+            secretKeyUseCase = fakeSecretKeyUseCase()
+        )
+        val result = usecase.testResult(uniqueCode = "$providerIdentifier-code")
+        assertTrue(result is TestResult.Complete)
+    }
+
+    @Test
+    fun `testResult returns VerificationRequired if RemoteTestResult status is VerificationRequired`() =
+        runBlocking {
+            val providerIdentifier = "provider"
+            val usecase = TestResultUseCase(
+                testProviderUseCase = fakeTestProviderUseCase(
+                    provider = getRemoteTestProvider(
+                        identifier = providerIdentifier
+                    )
+                ),
+                testProviderRepository = fakeTestProviderRepository(
+                    model =
+                    getRemoteTestResult(status = RemoteTestResult.Status.VERIFICATION_REQUIRED)
+                ),
+                coronaCheckRepository = fakeCoronaCheckRepository(),
+                commitmentMessageUseCase = fakeCommitmentMessageUsecase(),
+                secretKeyUseCase = fakeSecretKeyUseCase()
+            )
+            val result = usecase.testResult(uniqueCode = "$providerIdentifier-code")
+            assertTrue(result is TestResult.VerificationRequired)
+        }
+
+    @Test
+    fun `testResult returns InvalidToken if RemoteTestResult status is InvalidToken`() =
+        runBlocking {
+            val providerIdentifier = "provider"
+            val usecase = TestResultUseCase(
+                testProviderUseCase = fakeTestProviderUseCase(
+                    provider = getRemoteTestProvider(
+                        identifier = providerIdentifier
+                    )
+                ),
+                testProviderRepository = fakeTestProviderRepository(
+                    model =
+                    getRemoteTestResult(status = RemoteTestResult.Status.INVALID_TOKEN)
+                ),
+                coronaCheckRepository = fakeCoronaCheckRepository(),
+                commitmentMessageUseCase = fakeCommitmentMessageUsecase(),
+                secretKeyUseCase = fakeSecretKeyUseCase()
+            )
+            val result = usecase.testResult(uniqueCode = "$providerIdentifier-code")
+            assertTrue(result is TestResult.InvalidToken)
+        }
+
+    @Test
+    fun `testResult returns ServerError if HttpException is thrown`() =
+        runBlocking {
+            val providerIdentifier = "provider"
+            val usecase = TestResultUseCase(
+                testProviderUseCase = fakeTestProviderUseCase(
+                    provider = getRemoteTestProvider(
+                        identifier = providerIdentifier
+                    )
+                ),
+                testProviderRepository = object : TestProviderRepository {
+                    override suspend fun remoteTestResult(
+                        url: String,
+                        token: String,
+                        verifierCode: String?,
+                        signingCertificateBytes: ByteArray
+                    ): SignedResponseWithModel<RemoteTestResult> {
+                        throw HttpException(
+                            Response.error<String>(
+                                400, "".toResponseBody()
+                            )
+                        )
+                    }
+                },
+                coronaCheckRepository = fakeCoronaCheckRepository(),
+                commitmentMessageUseCase = fakeCommitmentMessageUsecase(),
+                secretKeyUseCase = fakeSecretKeyUseCase()
+            )
+            val result = usecase.testResult(uniqueCode = "$providerIdentifier-code")
+            assertTrue(result is TestResult.ServerError)
+        }
+
+    @Test
+    fun `testResult returns NetworkError if IOException is thrown`() =
+        runBlocking {
+            val providerIdentifier = "provider"
+            val usecase = TestResultUseCase(
+                testProviderUseCase = fakeTestProviderUseCase(
+                    provider = getRemoteTestProvider(
+                        identifier = providerIdentifier
+                    )
+                ),
+                testProviderRepository = object : TestProviderRepository {
+                    override suspend fun remoteTestResult(
+                        url: String,
+                        token: String,
+                        verifierCode: String?,
+                        signingCertificateBytes: ByteArray
+                    ): SignedResponseWithModel<RemoteTestResult> {
+                        throw IOException()
+                    }
+                },
+                coronaCheckRepository = fakeCoronaCheckRepository(),
+                commitmentMessageUseCase = fakeCommitmentMessageUsecase(),
+                secretKeyUseCase = fakeSecretKeyUseCase()
+            )
+            val result = usecase.testResult(uniqueCode = "$providerIdentifier-code")
+            assertTrue(result is TestResult.NetworkError)
+        }
+
+    @Test
+    fun `testResult returns Pending if RemoteTestResult status is Pending`() =
+        runBlocking {
+            val providerIdentifier = "provider"
+            val usecase = TestResultUseCase(
+                testProviderUseCase = fakeTestProviderUseCase(
+                    provider = getRemoteTestProvider(
+                        identifier = providerIdentifier
+                    )
+                ),
+                testProviderRepository = fakeTestProviderRepository(
+                    model =
+                    getRemoteTestResult(status = RemoteTestResult.Status.PENDING)
+                ),
+                coronaCheckRepository = fakeCoronaCheckRepository(),
+                commitmentMessageUseCase = fakeCommitmentMessageUsecase(),
+                secretKeyUseCase = fakeSecretKeyUseCase()
+            )
+            val result = usecase.testResult(uniqueCode = "$providerIdentifier-code")
+            assertTrue(result is TestResult.Pending)
+        }
+
+    private fun getRemoteTestProvider(identifier: String): RemoteTestProviders.Provider {
+        return RemoteTestProviders.Provider(
+            name = "dummy",
+            providerIdentifier = identifier,
+            resultUrl = "dummy",
+            publicKey = "dummy".toByteArray()
+        )
+    }
+
+    private fun getRemoteTestResult(status: RemoteTestResult.Status): SignedResponseWithModel<RemoteTestResult> {
+        return SignedResponseWithModel(
+            rawResponse = "dummy".toByteArray(), model = RemoteTestResult(
+                result = RemoteTestResult.Result(
+                    unique = "dummy",
+                    sampleDate = OffsetDateTime.now(),
+                    testType = "dummy",
+                    negativeResult = false
+                ),
+                providerIdentifier = "dummy",
+                status = status,
+                protocolVersion = "dummy"
+            )
+        )
     }
 
 }
