043533b0c8f5c09eacfa525ca88d9f5b2ed19d67
Bart Nijland <bartnijland91@gmail.com>
2021-02-26 14:29:26 +0100

    Add barista to holder app

---
+++ holder/src/test/java/nl/rijksoverheid/ctr/holder/myoverview/MyOverviewFragmentTest.kt
@@ -0,0 +1,18 @@
+package nl.rijksoverheid.ctr.holder.myoverview
+
+import org.junit.runner.RunWith
+import org.koin.test.AutoCloseKoinTest
+import org.robolectric.RobolectricTestRunner
+
+/*
+ *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *   SPDX-License-Identifier: EUPL-1.2
+ *
+ */
+@RunWith(RobolectricTestRunner::class)
+class MyOverviewFragmentTest: AutoCloseKoinTest() {
+
+
+}
