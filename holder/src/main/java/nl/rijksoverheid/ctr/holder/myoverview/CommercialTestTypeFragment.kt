f67da5f6190a9c4c48d438b6fa14cb80520278e6
Sander van 't Veer <sander@egeniq.com>
2021-02-26 14:45:24 +0100

    Enable QR Scanning in Holder

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/myoverview/CommercialTestTypeFragment.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/myoverview/CommercialTestTypeFragment.kt
@@ -2,13 +2,19 @@ package nl.rijksoverheid.ctr.holder.myoverview
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import nl.rijksoverheid.ctr.holder.R
 import nl.rijksoverheid.ctr.holder.databinding.FragmentCommercialTestTypeBinding
 import nl.rijksoverheid.ctr.holder.databinding.IncludeTestCodeTypeBinding
+import nl.rijksoverheid.ctr.shared.util.MLKitQrCodeScannerUtil
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
 
 /*
  *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
@@ -18,6 +24,16 @@ import nl.rijksoverheid.ctr.holder.databinding.IncludeTestCodeTypeBinding
  *
  */
 class CommercialTestTypeFragment : Fragment(R.layout.fragment_commercial_test_type) {
+
+    private val qrCodeScannerUtil: MLKitQrCodeScannerUtil by inject()
+
+    private val qrScanResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val scanResult = qrCodeScannerUtil.parseScanResult(it.data)
+            if (scanResult != null) {
+                Timber.d("Got scan result $scanResult")
+            }
+        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -29,7 +45,7 @@ class CommercialTestTypeFragment : Fragment(R.layout.fragment_commercial_test_ty
             R.drawable.ic_test_qr_code,
             R.string.commercial_test_type_qr_code_title
         ) {
-
+            qrCodeScannerUtil.launchScanner(requireActivity() as AppCompatActivity, qrScanResult)
         }
     }
 }
