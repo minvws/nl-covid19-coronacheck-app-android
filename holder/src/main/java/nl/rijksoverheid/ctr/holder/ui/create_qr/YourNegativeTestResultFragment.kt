5df3bc827f1cacbdccee37207656cb781b4c9a58
Bart Nijland <bartnijland91@gmail.com>
2021-03-12 13:36:04 +0100

    Remove QrCreatedFragment

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/YourNegativeTestResultFragment.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/YourNegativeTestResultFragment.kt
@@ -6,11 +6,11 @@ import androidx.activity.OnBackPressedCallback
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import nl.rijksoverheid.ctr.appconfig.AppConfigUtil
+import nl.rijksoverheid.ctr.design.ext.formatDateTime
 import nl.rijksoverheid.ctr.holder.BaseFragment
 import nl.rijksoverheid.ctr.holder.R
 import nl.rijksoverheid.ctr.holder.databinding.FragmentYourNegativeTestResultsBinding
 import nl.rijksoverheid.ctr.holder.usecase.SignedTestResult
-import nl.rijksoverheid.ctr.design.ext.formatDateTime
 import nl.rijksoverheid.ctr.shared.livedata.EventObserver
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ViewModelOwner
@@ -90,7 +90,7 @@ class YourNegativeTestResultFragment : BaseFragment(R.layout.fragment_your_negat
         viewModel.signedTestResult.observe(viewLifecycleOwner, EventObserver {
             when (it) {
                 is SignedTestResult.Complete -> {
-                    findNavController().navigate(YourNegativeTestResultFragmentDirections.actionCreateQr())
+                    findNavController().navigate(YourNegativeTestResultFragmentDirections.actionMyOverview())
                 }
                 is SignedTestResult.AlreadySigned -> {
                     findNavController().navigate(
