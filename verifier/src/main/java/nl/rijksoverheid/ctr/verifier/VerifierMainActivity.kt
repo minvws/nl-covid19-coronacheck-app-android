6725885f1a02b098c12ea4fee8057d3760f78bf3
Bart Nijland <bartnijland91@gmail.com>
2021-03-16 19:44:37 +0100

    Align drawer design

--- verifier/src/main/java/nl/rijksoverheid/ctr/verifier/VerifierMainActivity.kt
+++ verifier/src/main/java/nl/rijksoverheid/ctr/verifier/VerifierMainActivity.kt
@@ -134,7 +134,7 @@ class VerifierMainActivity : BaseActivity(R.id.nav_scan_qr) {
         binding.navView.menu.findItem(R.id.nav_scan_qr)
             .styleTitle(context, R.attr.textAppearanceHeadline6)
         binding.navView.menu.findItem(R.id.nav_support)
-            .styleTitle(context, R.attr.textAppearanceHeadline6)
+            .styleTitle(context, R.attr.textAppearanceBody1)
         binding.navView.menu.findItem(R.id.nav_about_this_app)
             .styleTitle(context, R.attr.textAppearanceBody1)
         binding.navView.menu.findItem(R.id.nav_give_us_feedback)
