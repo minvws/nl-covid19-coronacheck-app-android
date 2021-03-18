bebb63c23b87cb2b7432289065dcdf51fdad067e
Bart Nijland <bartnijland91@gmail.com>
2021-03-18 16:36:24 +0100

    Fix VerifierApplication

--- verifier/src/main/java/nl/rijksoverheid/ctr/verifier/VerifierApplication.kt
+++ verifier/src/main/java/nl/rijksoverheid/ctr/verifier/VerifierApplication.kt
@@ -21,7 +21,7 @@ import org.koin.core.context.startKoin
  *   SPDX-License-Identifier: EUPL-1.2
  *
  */
-class VerifierApplication : SharedApplication(), CoronaCheckApp, AppStatusStringProvider {
+class VerifierApplication : SharedApplication(), CoronaCheckApp {
 
     private val loadPublicKeysUseCase: LoadPublicKeysUseCase by inject()
     private val cachedAppConfigUseCase: CachedAppConfigUseCase by inject()
@@ -104,20 +104,6 @@ class VerifierApplication : SharedApplication(), CoronaCheckApp, AppStatusString
             privacyPolicyStringResource = R.string.privacy_policy_description,
             privacyPolicyCheckboxStringResource = R.string.privacy_policy_checkbox_text,
             onboardingNextButtonStringResource = R.string.onboarding_next
-        )
-    }
-
-    override fun getAppStatusStrings(): AppStatusStringProvider.AppStatusStrings {
-        return AppStatusStringProvider.AppStatusStrings(
-            appStatusDeactivatedTitle = R.string.app_status_deactivated_title,
-            appStatusDeactivatedMessage = R.string.app_status_deactivated_message,
-            appStatusDeactivatedAction = R.string.app_status_deactivated_action,
-            appStatusUpdateRequiredAction = R.string.app_status_update_required_action,
-            appStatusUpdateRequiredMessage = R.string.app_status_update_required_message,
-            appStatusUpdateRequiredTitle = R.string.app_status_update_required_title,
-            appStatusInternetRequiredTitle = R.string.app_status_internet_required_title,
-            appStatusInternetRequiredMessage = R.string.app_status_internet_required_message,
-            appStatusInternetRequiredAction = R.string.app_status_internet_required_action
         )
     }
 }
