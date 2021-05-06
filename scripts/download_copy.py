2d85392bd75230c63fce651de690045eb9014cad
Bart Nijland <bartnijland91@gmail.com>
2021-05-06 09:58:03 +0200

    English copy export

--- scripts/download_copy.py
+++ scripts/download_copy.py
@@ -8,15 +8,22 @@ if os.getenv('CORONACHECK_ONESKY_SECRET') is not None:
 	secretkey = os.getenv('CORONACHECK_ONESKY_SECRET')
 	timestamp = time.time()
 	devhash = hashlib.md5((str(timestamp) + secretkey).encode('utf-8')).hexdigest()
-	payload = {'api_key': publickey, 'timestamp': timestamp, 'dev_hash': devhash, 'locale': "nl", "source_file_name": "strings.xml", "export_file_name": "strings.xml"} 
 
-	print("Downloading copy for Holder")
-	r = requests.get("https://platform.api.onesky.io/1/projects/380524/translations", params = payload)
+	print("Downloading NL copy for Holder")
+	r = requests.get("https://platform.api.onesky.io/1/projects/380524/translations", params = {'api_key': publickey, 'timestamp': timestamp, 'dev_hash': devhash, 'locale': "nl", "source_file_name": "strings.xml", "export_file_name": "strings.xml"})
 	open('../holder/src/main/res/values/strings.xml', 'wb').write(r.content)
 
-	print("Downloading copy for Verifier")
-	r = requests.get("https://platform.api.onesky.io/1/projects/380530/translations", params = payload)
+	print("Downloading EN copy for Holder")
+	r = requests.get("https://platform.api.onesky.io/1/projects/380524/translations", params = {'api_key': publickey, 'timestamp': timestamp, 'dev_hash': devhash, 'locale': "en", "source_file_name": "strings.xml", "export_file_name": "strings.xml"})
+	open('../holder/src/main/res/values-en/strings.xml', 'wb').write(r.content)
+
+	print("Downloading NL copy for Verifier")
+	r = requests.get("https://platform.api.onesky.io/1/projects/380530/translations", params = {'api_key': publickey, 'timestamp': timestamp, 'dev_hash': devhash, 'locale': "nl", "source_file_name": "strings.xml", "export_file_name": "strings.xml"})
 	open('../verifier/src/main/res/values/strings.xml', 'wb').write(r.content)
+
+	print("Downloading EN copy for Verifier")
+	r = requests.get("https://platform.api.onesky.io/1/projects/380530/translations", params = {'api_key': publickey, 'timestamp': timestamp, 'dev_hash': devhash, 'locale': "en", "source_file_name": "strings.xml", "export_file_name": "strings.xml"})
+	open('../verifier/src/main/res/values-en/strings.xml', 'wb').write(r.content)
 
 	print("Finished downloading copy")
 else:
