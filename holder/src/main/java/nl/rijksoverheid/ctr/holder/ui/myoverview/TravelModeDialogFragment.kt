e61277693a6a49854e7b2ed6920d37b34255af75
Sander van 't Veer <sander@egeniq.com>
2021-05-17 13:21:28 +0200

    Travel mode fragment base

---
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/myoverview/TravelModeDialogFragment.kt
@@ -0,0 +1,50 @@
+/*
+ *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *   SPDX-License-Identifier: EUPL-1.2
+ *
+ */
+
+package nl.rijksoverheid.ctr.holder.ui.myoverview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.AccessibilityDelegateCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
+import nl.rijksoverheid.ctr.design.ExpandedBottomSheetDialogFragment
+import nl.rijksoverheid.ctr.holder.databinding.FragmentTravelModeBinding
+
+class TravelModeDialogFragment : ExpandedBottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return FragmentTravelModeBinding.inflate(inflater).root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentTravelModeBinding.bind(view)
+
+        binding.close.setOnClickListener {
+            dismiss()
+        }
+
+        ViewCompat.setAccessibilityDelegate(binding.close, object : AccessibilityDelegateCompat() {
+            override fun onInitializeAccessibilityNodeInfo(
+                host: View?,
+                info: AccessibilityNodeInfoCompat?
+            ) {
+                info?.setTraversalBefore(binding.description)
+                super.onInitializeAccessibilityNodeInfo(host, info)
+            }
+        })
+    }
+}
