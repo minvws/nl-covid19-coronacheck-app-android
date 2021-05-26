b7bb5c8d17f48301e2b9bce22b780a3f4e15343b
Sander van 't Veer <sander@egeniq.com>
2021-05-26 14:30:49 +0200

    Remove 'Waar ben je getest' screen

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/myoverview/MyOverviewFragment.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/myoverview/MyOverviewFragment.kt
@@ -167,7 +167,7 @@ class MyOverviewFragment : Fragment(R.layout.fragment_my_overview) {
             buttonText = if (localTestResult == null) R.string.my_overview_no_qr_make_qr_button else R.string.my_overview_no_qr_replace_qr_button,
             onButtonClick = {
                 findNavControllerSafety(R.id.nav_my_overview)?.navigate(
-                    MyOverviewFragmentDirections.actionChooseProvider()
+                    MyOverviewFragmentDirections.actionCommercialTestCode()
                 )
             }
         ))
