1a7664db1fe081146bb93851bde992c2f521fd6e
Giorgos Papadopoulos <giorgos@giorgos.nl>
2021-06-05 12:50:12 +0200

    store also the eu_keys key

--- appconfig/src/main/java/nl/rijksoverheid/ctr/appconfig/api/model/PublicKeys.kt
+++ appconfig/src/main/java/nl/rijksoverheid/ctr/appconfig/api/model/PublicKeys.kt
@@ -3,6 +3,7 @@ package nl.rijksoverheid.ctr.appconfig.api.model
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import nl.rijksoverheid.ctr.shared.models.JSON
+import org.json.JSONObject
 
 /*
  *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
@@ -13,7 +14,8 @@ import nl.rijksoverheid.ctr.shared.models.JSON
  */
 @JsonClass(generateAdapter = true)
 data class PublicKeys(
-    @Json(name = "cl_keys") val clKeys: List<ClKey>
+    @Json(name = "cl_keys") val clKeys: List<ClKey>,
+    @Json(name = "eu_keys") val euKeys: JSONObject,
 ) : JSON() {
     @JsonClass(generateAdapter = true)
     data class ClKey(
