353ea175c3ccdb31ded1b949e3990047686cb7cb
Bart Nijland <bartnijland91@gmail.com>
2021-06-07 15:21:37 +0200

    Do not enable show qr button if there are no valid origins

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/usecases/GetMyOverviewItemsUseCase.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/usecases/GetMyOverviewItemsUseCase.kt
@@ -140,8 +140,8 @@ class GetMyOverviewItemsUseCaseImpl(private val holderDatabase: HolderDatabase,
                     origins = greenCard.origins
                 )
 
-                // Filter out origins that are expired
-                val filteredOriginStates = originStates.filter { it !is OriginState.Expired }
+                // Check if we have any valid origins
+                val hasValidOriginStates = originStates.any { it is OriginState.Valid }
 
                 val euLaunchDate = OffsetDateTime.parse(cachedAppConfigUseCase.getCachedAppConfig()!!.euLaunchDate, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
 
@@ -150,7 +150,7 @@ class GetMyOverviewItemsUseCaseImpl(private val holderDatabase: HolderDatabase,
                 // More our credential to a more readable state
                 val credentialState = when {
                     activeCredential == null -> CredentialState.NoCredential
-                    filteredOriginStates.isEmpty() -> CredentialState.NoCredential
+                    !hasValidOriginStates -> CredentialState.NoCredential
                     launchDate.isAfter(OffsetDateTime.now()) -> CredentialState.NoCredential
                     else -> CredentialState.HasCredential(activeCredential)
                 }
@@ -158,7 +158,7 @@ class GetMyOverviewItemsUseCaseImpl(private val holderDatabase: HolderDatabase,
                 // Show green card
                 GreenCardItem(
                     greenCard = greenCard,
-                    originStates = filteredOriginStates,
+                    originStates = originStates,
                     credentialState = credentialState,
                     launchDate = launchDate,
                 )
