0171723ac0e2d9b70c8093480ec22a2dcfa60f96
Bart Nijland <bartnijland91@gmail.com>
2021-06-09 18:58:39 +0200

    Only set the enabled state background color of the button

--- design/src/main/java/nl/rijksoverheid/ctr/design/ext/ButtonExt.kt
+++ design/src/main/java/nl/rijksoverheid/ctr/design/ext/ButtonExt.kt
@@ -2,11 +2,28 @@
 
 package nl.rijksoverheid.ctr.design.ext
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.widget.Button
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 import nl.rijksoverheid.ctr.design.R
 
-fun Button.setButtonColor(@ColorRes color: Int) {
-    this.backgroundTintList = ContextCompat.getColorStateList(this.context, color)
+/**
+ * Sets only enabled state background color of the button
+ */
+fun Button.setEnabledButtonColor(@ColorRes color: Int) {
+    this.backgroundTintList?.let { colorStateList ->
+        val states = arrayOf(
+            intArrayOf(android.R.attr.state_enabled),
+            intArrayOf(-android.R.attr.state_enabled)
+        )
+
+        val colors = intArrayOf(
+            ContextCompat.getColor(this.context, color),
+            colorStateList.getColorForState(intArrayOf(-android.R.attr.state_enabled), Color.BLACK)
+        )
+
+        this.backgroundTintList = ColorStateList(states, colors)
+    }
 }
