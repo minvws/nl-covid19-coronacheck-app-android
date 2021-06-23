4ef50e267d5dab409e7531d2bb36d60ec3a83a78
Giorgos Papadopoulos <giorgos@giorgos.nl>
2021-06-23 11:33:48 +0200

    update tests

--- appconfig/src/test/java/nl/rijksoverheid/ctr/appconfig/AppConfigViewModelTest.kt
+++ appconfig/src/test/java/nl/rijksoverheid/ctr/appconfig/AppConfigViewModelTest.kt
@@ -8,14 +8,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.setMain
-import nl.rijksoverheid.ctr.appconfig.api.model.AppConfig
-import nl.rijksoverheid.ctr.appconfig.api.model.PublicKeys
 import nl.rijksoverheid.ctr.appconfig.models.AppStatus
 import nl.rijksoverheid.ctr.appconfig.models.ConfigResult
 import nl.rijksoverheid.ctr.appconfig.persistence.AppConfigStorageManager
 import nl.rijksoverheid.ctr.appconfig.usecases.AppConfigUseCase
 import nl.rijksoverheid.ctr.appconfig.usecases.AppStatusUseCase
-import nl.rijksoverheid.ctr.appconfig.usecases.LoadPublicKeysUseCase
 import nl.rijksoverheid.ctr.appconfig.usecases.PersistConfigUseCase
 import nl.rijksoverheid.ctr.shared.MobileCoreWrapper
 import okio.BufferedSource
@@ -36,21 +33,20 @@ class AppConfigViewModelTest {
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
+    private val cacheDirPath = "/cache"
+
     private val appConfigUseCase: AppConfigUseCase = mockk(relaxed = true)
     private val appStatusUseCase: AppStatusUseCase = mockk(relaxed = true)
     private val persistConfigUseCase: PersistConfigUseCase = mockk(relaxed = true)
-    private val loadPublicKeyUseCase: LoadPublicKeysUseCase = mockk(relaxed = true)
     private val appConfigStorageManager: AppConfigStorageManager = mockk(relaxed = true)
-    private val cachedAppConfigUseCase: CachedAppConfigUseCase = mockk(relaxed = true)
-    private val appConfigViewModel = AppConfigViewModelImpl(
+
+    private fun appConfigViewModel(isVerifier: Boolean = false) = AppConfigViewModelImpl(
         appConfigUseCase = appConfigUseCase,
         appStatusUseCase = appStatusUseCase,
         persistConfigUseCase = persistConfigUseCase,
-        loadPublicKeysUseCase = loadPublicKeyUseCase,
         appConfigStorageManager = appConfigStorageManager,
-        cachedAppConfigUseCase = cachedAppConfigUseCase,
-        cacheDirPath = "",
-        isVerifierApp = false,
+        cacheDirPath = cacheDirPath,
+        isVerifierApp = isVerifier,
         versionCode = 0
     )
     private val mobileCoreWrapper: MobileCoreWrapper = mockk(relaxed = true)
@@ -61,21 +57,7 @@ class AppConfigViewModelTest {
     }
 
     @Test
-    fun `refresh calls correct usecases when success`() = runBlocking {
-        val appConfig = AppConfig(
-            minimumVersion = 0,
-            appDeactivated = false,
-            informationURL = "dummy",
-            configTtlSeconds = 0,
-            maxValidityHours = 0,
-            euLaunchDate = "",
-            credentialRenewalDays = 0,
-            domesticCredentialValidity = 0,
-            testEventValidity = 0,
-            recoveryEventValidity = 0,
-            temporarilyDisabled = false,
-            requireUpdateBefore = 0
-        )
+    fun `given a happy flow on holder, then calls correct usecases only`() = runBlocking {
         val appConfigContents = "app config contents"
 
         val publicKeys = mockk<BufferedSource>()
@@ -88,14 +70,40 @@ class AppConfigViewModelTest {
                 publicKeys = publicKeysContents
             )
         }
-
+        coEvery { appConfigStorageManager.areConfigFilesPresent() } returns true
         coEvery { appStatusUseCase.get(any(), any()) } answers { AppStatus.NoActionRequired }
-        coEvery { cachedAppConfigUseCase.getCachedPublicKeys() } returns publicKeys
+        coEvery { mobileCoreWrapper.initializeHolder(cacheDirPath) } returns null
 
-        appConfigViewModel.refresh(mobileCoreWrapper)
+        appConfigViewModel().refresh(mobileCoreWrapper)
 
         coVerify { persistConfigUseCase.persist(appConfigContents, publicKeysContents) }
-        coVerify { loadPublicKeyUseCase.load(publicKeys) }
+        coVerify { mobileCoreWrapper.initializeHolder(cacheDirPath) }
+        coVerify(exactly = 0) { mobileCoreWrapper.initializeVerifier(cacheDirPath) }
+    }
+
+    @Test
+    fun `given a happy flow on verifier, then calls correct usecases only`() = runBlocking {
+        val appConfigContents = "app config contents"
+
+        val publicKeys = mockk<BufferedSource>()
+        val publicKeysContents = "file contents"
+        coEvery { publicKeys.readUtf8() } returns publicKeysContents
+
+        coEvery { appConfigUseCase.get() } answers {
+            ConfigResult.Success(
+                appConfig = appConfigContents,
+                publicKeys = publicKeysContents
+            )
+        }
+        coEvery { appConfigStorageManager.areConfigFilesPresent() } returns true
+        coEvery { appStatusUseCase.get(any(), any()) } answers { AppStatus.NoActionRequired }
+        coEvery { mobileCoreWrapper.initializeVerifier(cacheDirPath) } returns null
+
+        appConfigViewModel(true).refresh(mobileCoreWrapper)
+
+        coVerify { persistConfigUseCase.persist(appConfigContents, publicKeysContents) }
+        coVerify(exactly = 0) { mobileCoreWrapper.initializeHolder(cacheDirPath) }
+        coVerify { mobileCoreWrapper.initializeVerifier(cacheDirPath) }
     }
 
     @Test
@@ -106,31 +114,14 @@ class AppConfigViewModelTest {
 
         coEvery { appStatusUseCase.get(any(), any()) } answers { AppStatus.InternetRequired }
 
-        appConfigViewModel.refresh(mobileCoreWrapper)
+        val viewModel = appConfigViewModel()
+        viewModel.refresh(mobileCoreWrapper)
 
-        Assert.assertEquals(appConfigViewModel.appStatusLiveData.value, AppStatus.InternetRequired)
+        Assert.assertEquals(viewModel.appStatusLiveData.value, AppStatus.InternetRequired)
     }
 
     @Test
     fun `refresh with no config files in verifier app emits internet required status`() = runBlocking {
-        val appConfigViewModel = AppConfigViewModelImpl(
-            appConfigUseCase = appConfigUseCase,
-            appStatusUseCase = appStatusUseCase,
-            persistConfigUseCase = persistConfigUseCase,
-            loadPublicKeysUseCase = loadPublicKeyUseCase,
-            appConfigStorageManager = appConfigStorageManager,
-            cachedAppConfigUseCase = cachedAppConfigUseCase,
-            cacheDirPath = "",
-            isVerifierApp = true,
-            versionCode = 0
-        )
-        val appConfig = AppConfig(
-            minimumVersion = 0,
-            appDeactivated = false,
-            informationURL = "dummy",
-            configTtlSeconds = 0,
-            maxValidityHours = 0
-        )
         val appConfigContents = "app config contents"
 
         val publicKeys = mockk<BufferedSource>()
@@ -147,38 +138,9 @@ class AppConfigViewModelTest {
         coEvery { appStatusUseCase.get(any(), any()) } answers { AppStatus.NoActionRequired }
         coEvery { appConfigStorageManager.areConfigFilesPresent() } returns false
 
-        appConfigViewModel.refresh(mobileCoreWrapper)
+        val viewModel = appConfigViewModel(true)
+        viewModel.refresh(mobileCoreWrapper)
 
-        Assert.assertEquals(appConfigViewModel.appStatusLiveData.value, AppStatus.InternetRequired)
-    }
-
-    @Test
-    fun `refresh in the holder app has no interaction with config files and initialise the verifier`() {
-        val appConfig = AppConfig(
-            minimumVersion = 0,
-            appDeactivated = false,
-            informationURL = "dummy",
-            configTtlSeconds = 0,
-            maxValidityHours = 0
-        )
-        val appConfigContents = "app config contents"
-
-        val publicKeys = mockk<BufferedSource>()
-        val publicKeysContents = "file contents"
-        coEvery { publicKeys.readUtf8() } returns publicKeysContents
-
-        coEvery { appConfigUseCase.get() } answers {
-            ConfigResult.Success(
-                appConfig = appConfigContents,
-                publicKeys = publicKeysContents
-            )
-        }
-
-        coEvery { appStatusUseCase.get(any(), any()) } answers { AppStatus.NoActionRequired }
-
-        appConfigViewModel.refresh(mobileCoreWrapper)
-
-        coVerify(exactly = 0) { mobileCoreWrapper.initializeVerifier(any()) }
-        coVerify(exactly = 0) { appConfigStorageManager.areConfigFilesPresent() }
+        Assert.assertEquals(viewModel.appStatusLiveData.value, AppStatus.InternetRequired)
     }
 }
