07095a9ac8c935149a4816ef75b6ff3c607ec0eb
Giorgos Papadopoulos <giorgos@giorgos.nl>
2021-06-29 10:51:09 +0200

    signer version 1.1.6

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/api/HolderApiClient.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/api/HolderApiClient.kt
@@ -23,7 +23,7 @@ interface HolderApiClient {
     @SignedRequest
     suspend fun getPrepareIssue(): RemotePrepareIssue
 
-    @POST("holder/get_credentials")
+    @POST("holder/get_credentials2")
     @SignedRequest
     suspend fun getCredentials(
         @Body data: GetCredentialsPostData
