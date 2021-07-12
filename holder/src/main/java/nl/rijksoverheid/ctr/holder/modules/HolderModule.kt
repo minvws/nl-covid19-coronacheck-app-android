369f3d0b52d8d0bbc0ac5d8b4157d9871674be7c
Bart Nijland <bartnijland91@gmail.com>
2021-07-12 12:39:38 +0200

    Correctly handle refresh states

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/modules/HolderModule.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/modules/HolderModule.kt
@@ -122,7 +122,7 @@ fun holderModule(baseUrl: String) = module {
         )
     }
     factory<GetMyOverviewItemsUseCase> {
-        GetMyOverviewItemsUseCaseImpl(get(), get(), get(), get(), get())
+        GetMyOverviewItemsUseCaseImpl(get(), get(), get(), get())
     }
     factory<TokenValidatorUtil> { TokenValidatorUtilImpl() }
     factory<CredentialUtil> { CredentialUtilImpl(Clock.systemUTC()) }
