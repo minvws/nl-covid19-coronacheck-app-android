99c32587b76f216349f063aca26f324a98161621
Giorgos Papadopoulos <giorgos@giorgos.nl>
2021-07-19 14:48:50 +0200

    separate exception for the library errors

--- shared/src/main/java/nl/rijksoverheid/ctr/shared/ext/ClmobileExt.kt
+++ shared/src/main/java/nl/rijksoverheid/ctr/shared/ext/ClmobileExt.kt
@@ -9,9 +9,33 @@ import org.json.JSONObject
  *   SPDX-License-Identifier: EUPL-1.2
  *
  */
+
+sealed class MobileCoreException(message: String): Exception(message) {
+    class MobileCoreException1(message: String): MobileCoreException(message)
+    class MobileCoreException2(message: String): MobileCoreException(message)
+    class MobileCoreException3(message: String): MobileCoreException(message)
+    class MobileCoreException4(message: String): MobileCoreException(message)
+    class MobileCoreException5(message: String): MobileCoreException(message)
+    class MobileCoreException6(message: String): MobileCoreException(message)
+    class MobileCoreException7(message: String): MobileCoreException(message)
+    class MobileCoreException8(message: String): MobileCoreException(message)
+    class MobileCoreException9(message: String): MobileCoreException(message)
+}
+
 fun mobilecore.Result.successString(): String {
-    if (this.error.isNotEmpty()) {
-        throw Exception(this.error)
+    if (error.isNotEmpty()) {
+        throw when {
+            error.contains("CreateCommitmentMessage should be called before CreateCredentials") -> MobileCoreException.MobileCoreException1(error)
+            error.contains("Could not unmarshal create credential messages") -> MobileCoreException.MobileCoreException2(error)
+            error.contains("More credentials are being issued than there are proof builders") -> MobileCoreException.MobileCoreException3(error)
+            error.contains("Proof of correctness on signature does not verify") -> MobileCoreException.MobileCoreException4(error)
+            error.contains("The Signature on the attributes is not correct") -> MobileCoreException.MobileCoreException5(error)
+            error.contains("Could not construct credential") -> MobileCoreException.MobileCoreException6(error)
+            error.contains("Could not read freshly contructed credential") -> MobileCoreException.MobileCoreException7(error)
+            error.contains("Invalid credential version in freshly constructed credential") -> MobileCoreException.MobileCoreException8(error)
+            error.contains("Could not create credentials") -> MobileCoreException.MobileCoreException9(error)
+            else -> Exception(this.error)
+        }
     } else {
         return String(this.value)
     }
