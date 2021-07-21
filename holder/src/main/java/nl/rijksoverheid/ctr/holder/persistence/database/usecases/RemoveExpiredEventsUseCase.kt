57cda8f5930e73a49c27221cd8fae2fd8ded9a2e
Bart Nijland <bartnijland91@gmail.com>
2021-07-21 16:35:14 +0200

    Properly remove dcc events

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/persistence/database/usecases/RemoveExpiredEventsUseCase.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/persistence/database/usecases/RemoveExpiredEventsUseCase.kt
@@ -4,6 +4,7 @@ import nl.rijksoverheid.ctr.holder.persistence.CachedAppConfigUseCase
 import nl.rijksoverheid.ctr.holder.persistence.database.HolderDatabase
 import nl.rijksoverheid.ctr.holder.persistence.database.entities.OriginType
 import nl.rijksoverheid.ctr.holder.ui.create_qr.models.RemoteConfigProviders
+import timber.log.Timber
 import java.time.Clock
 import java.time.OffsetDateTime
 
@@ -20,34 +21,21 @@ class RemoveExpiredEventsUseCaseImpl(
     override suspend fun execute() {
         val events = holderDatabase.eventGroupDao().getAll()
         events.forEach {
-            val expireHours =
-                if (it.providerIdentifier == RemoteConfigProviders.EventProvider.PROVIDER_IDENTIFIER_DCC) {
-                    // If this is a dcc scanned event, the event validity is equals to the maxIssuedAt
-                    0
-                } else {
-                    // If it's not, we have a bit of control and can add validity to it from remote config
-                    getValidityHoursForOriginType(
-                        originType = it.type
-                    )
+            val cachedAppConfig = cachedAppConfigUseCase.getCachedAppConfig()
+            val expirationTime = when(it.type) {
+                is OriginType.Vaccination -> {
+                    cachedAppConfig.vaccinationEventValidity
                 }
+                is OriginType.Test -> {
+                    cachedAppConfig.testEventValidity
+                }
+                is OriginType.Recovery -> {
+                    cachedAppConfig.recoveryEventValidity
+                }
+            }
 
-            if (it.maxIssuedAt.plusHours(expireHours.toLong()) <= OffsetDateTime.now(clock)) {
+            if (it.maxIssuedAt.plusHours(expirationTime.toLong()) <= OffsetDateTime.now(clock)) {
                 holderDatabase.eventGroupDao().delete(it)
-            }
-        }
-    }
-
-    private fun getValidityHoursForOriginType(originType: OriginType): Int {
-        val cachedAppConfig = cachedAppConfigUseCase.getCachedAppConfig()
-        return when (originType) {
-            is OriginType.Vaccination -> {
-                cachedAppConfig.vaccinationEventValidity
-            }
-            is OriginType.Test -> {
-                cachedAppConfig.testEventValidity
-            }
-            is OriginType.Recovery -> {
-                cachedAppConfig.recoveryEventValidity
             }
         }
     }
