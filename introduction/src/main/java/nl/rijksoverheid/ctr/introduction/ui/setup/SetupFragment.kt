7b229a147ea7a6bbbc904d360c92113270dc36b0
Aiden Shi <aiden@aidenshi.nl>
2021-08-18 13:23:11 +0200

    show dialog from setup fragment

--- introduction/src/main/java/nl/rijksoverheid/ctr/introduction/ui/setup/SetupFragment.kt
+++ introduction/src/main/java/nl/rijksoverheid/ctr/introduction/ui/setup/SetupFragment.kt
@@ -9,10 +9,12 @@ import androidx.navigation.fragment.navArgs
 import nl.rijksoverheid.ctr.appconfig.AppConfigViewModel
 import nl.rijksoverheid.ctr.appconfig.AppStatusFragment
 import nl.rijksoverheid.ctr.appconfig.models.AppStatus
+import nl.rijksoverheid.ctr.design.utils.DialogUtil
 import nl.rijksoverheid.ctr.introduction.R
 import nl.rijksoverheid.ctr.shared.MobileCoreWrapper
 import nl.rijksoverheid.ctr.shared.livedata.EventObserver
 import nl.rijksoverheid.ctr.shared.utils.Accessibility
+import nl.rijksoverheid.ctr.shared.utils.IntentUtil
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -28,6 +30,8 @@ class SetupFragment : Fragment(R.layout.fragment_setup) {
     private val args: SetupFragmentArgs by navArgs()
     private val appStatusViewModel: AppConfigViewModel by viewModel()
     private val mobileCoreWrapper: MobileCoreWrapper by inject()
+    private val dialogUtil: DialogUtil by inject()
+    private val intentUtil: IntentUtil by inject()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -41,12 +45,36 @@ class SetupFragment : Fragment(R.layout.fragment_setup) {
 
     private fun setObservers() {
         appStatusViewModel.appStatusLiveData.observe(viewLifecycleOwner, EventObserver {
-            if (it is AppStatus.NoActionRequired || it is AppStatus.UpdateRecommended) {
-                findNavController().navigate(SetupFragmentDirections.actionOnboarding(args.introductionData))
-            } else {
-                val bundle = bundleOf(AppStatusFragment.EXTRA_APP_STATUS to it)
-                findNavController().navigate(R.id.action_app_status, bundle)
+            when (it) {
+                is AppStatus.UpdateRecommended -> showRecommendedUpdateDialog()
+                is AppStatus.NoActionRequired -> navigateToOnboarding()
+                else -> navigateToAppStatus(it)
             }
         })
+    }
+
+    private fun navigateToAppStatus(appStatus: AppStatus) {
+        findNavController().navigate(
+            R.id.action_app_status,
+            bundleOf(AppStatusFragment.EXTRA_APP_STATUS to appStatus)
+        )
+    }
+
+    private fun navigateToOnboarding() {
+        findNavController().navigate(
+            SetupFragmentDirections.actionOnboarding(args.introductionData)
+        )
+    }
+
+    private fun showRecommendedUpdateDialog() {
+        dialogUtil.presentDialog(
+            context = requireContext(),
+            title = R.string.app_status_update_recommended_title,
+            message = getString(R.string.app_status_update_recommended_message),
+            positiveButtonText = R.string.app_status_update_recommended_action,
+            positiveButtonCallback = { intentUtil.openPlayStore(requireContext()) },
+            negativeButtonText = R.string.app_status_update_recommended_dismiss_action,
+            onDismissCallback = { navigateToOnboarding() }
+        )
     }
 }
