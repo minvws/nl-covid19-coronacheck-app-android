ed20ca61d365ad4873ed726997ff2c97025a788c
Bart Nijland <bartnijland91@gmail.com>
2021-08-23 14:20:48 +0200

    Fix verifier

--- verifier/src/main/java/nl/rijksoverheid/ctr/verifier/VerifierStep.kt
+++ verifier/src/main/java/nl/rijksoverheid/ctr/verifier/VerifierStep.kt
@@ -2,7 +2,7 @@ package nl.rijksoverheid.ctr.verifier
 
 import nl.rijksoverheid.ctr.api.models.Step
 
-sealed class VerifierStep(val code: Int) : Step {
+sealed class VerifierStep(override val code: Int) : Step(code) {
     object ConfigurationNetworkRequest : VerifierStep(10)
     object PublicKeysNetworkRequest : VerifierStep(20)
 }
