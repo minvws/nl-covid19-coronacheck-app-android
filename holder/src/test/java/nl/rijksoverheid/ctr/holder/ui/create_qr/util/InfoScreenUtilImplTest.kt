821dadcd644a11c21c3162fe6037bf57b3a58eab
Bart Nijland <bartnijland91@gmail.com>
2021-08-27 13:49:23 +0200

    Fix test

--- holder/src/test/java/nl/rijksoverheid/ctr/holder/ui/create_qr/util/InfoScreenUtilImplTest.kt
+++ holder/src/test/java/nl/rijksoverheid/ctr/holder/ui/create_qr/util/InfoScreenUtilImplTest.kt
@@ -8,9 +8,11 @@
 
 package nl.rijksoverheid.ctr.holder.ui.create_qr.util
 
+import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import io.mockk.mockk
 import junit.framework.Assert.assertEquals
+import nl.rijksoverheid.ctr.holder.R
 import nl.rijksoverheid.ctr.holder.fakeCachedAppConfigUseCase
 import org.json.JSONObject
 import org.junit.Test
@@ -23,6 +25,7 @@ import java.util.*
 @RunWith(RobolectricTestRunner::class)
 @Config(qualifiers = "nl-land")
 class InfoScreenUtilImplTest: AutoCloseKoinTest() {
+
 
     private val infoScreenUtil =
         InfoScreenUtilImpl(ApplicationProvider.getApplicationContext(), mockk(), fakeCachedAppConfigUseCase())
@@ -44,4 +47,28 @@ class InfoScreenUtilImplTest: AutoCloseKoinTest() {
         val dutchString = infoScreenUtil.getCountry("nl", Locale("en", "en"))
         assertEquals("Netherlands", dutchString)
     }
+
+    @Test
+    fun `getForEuropeanTestQr returns correct info`() {
+        val jsonString = "{\"credentialVersion\":1,\"issuer\":\"NL\",\"issuedAt\":1628579448,\"expirationTime\":1630998658,\"dcc\":{\"ver\":\"1.3.0\",\"dob\":\"1991-08-18\",\"nam\":{\"fn\":\"ten Bouwer\",\"fnt\":\"TEN<BOUWER\",\"gn\":\"Bob\",\"gnt\":\"BOB\"},\"v\":null,\"t\":[{\"tg\":\"840539006\",\"tt\":\"LP6464-4\",\"nm\":\"\",\"ma\":\"\",\"sc\":\"2021-08-10T03:10:00+00:00\",\"dr\":\"\",\"tr\":\"260415000\",\"tc\":\"Facility approved by the State of The Netherlands\",\"co\":\"NL\",\"is\":\"Ministry of Health Welfare and Sport\",\"ci\":\"URN:UCI:01:NL:PJ7JLSZ4KRGX5O2E7OD342#E\"}],\"r\":null}}"
+        val json = JSONObject(jsonString)
+
+        val infoScreen = infoScreenUtil.getForEuropeanTestQr(json)
+
+        assertEquals((ApplicationProvider.getApplicationContext() as Context).getString(R.string.qr_explanation_description_eu_test,
+            "ten Bouwer, Bob",
+            "18-08-1991",
+            "COVID-19",
+            "LP6464-4",
+            "",
+            "10-08-2021",
+            "negatief (geen corona)",
+            "Facility approved by the State of The Netherlands",
+            "",
+            "Netherlands",
+            "Ministerie van VWS / Ministry of Health, Welfare and Sport",
+            "URN:UCI:01:NL:PJ7JLSZ4KRGX5O2E7OD342#E"
+            ), infoScreen.description)
+    }
+
 }
