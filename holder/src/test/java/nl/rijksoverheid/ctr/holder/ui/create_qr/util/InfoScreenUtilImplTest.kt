a252370a3fcd0a25a1dbb24494718cddb85f94ef
Bart Nijland <bartnijland91@gmail.com>
2021-08-27 13:43:13 +0200

    Fix test

--- holder/src/test/java/nl/rijksoverheid/ctr/holder/ui/create_qr/util/InfoScreenUtilImplTest.kt
+++ holder/src/test/java/nl/rijksoverheid/ctr/holder/ui/create_qr/util/InfoScreenUtilImplTest.kt
@@ -44,16 +44,4 @@ class InfoScreenUtilImplTest: AutoCloseKoinTest() {
         val dutchString = infoScreenUtil.getCountry("nl", Locale("en", "en"))
         assertEquals("Netherlands", dutchString)
     }
-
-    @Test
-    fun `getForEuropeanTestQr returns correct info`() {
-        val jsonString = "{\"credentialVersion\":1,\"issuer\":\"NL\",\"issuedAt\":1628579448,\"expirationTime\":1630998658,\"dcc\":{\"ver\":\"1.3.0\",\"dob\":\"1991-08-18\",\"nam\":{\"fn\":\"ten Bouwer\",\"fnt\":\"TEN<BOUWER\",\"gn\":\"Bob\",\"gnt\":\"BOB\"},\"v\":null,\"t\":[{\"tg\":\"840539006\",\"tt\":\"LP6464-4\",\"nm\":\"\",\"ma\":\"\",\"sc\":\"2021-08-10T03:10:00+00:00\",\"dr\":\"\",\"tr\":\"260415000\",\"tc\":\"Facility approved by the State of The Netherlands\",\"co\":\"NL\",\"is\":\"Ministry of Health Welfare and Sport\",\"ci\":\"URN:UCI:01:NL:PJ7JLSZ4KRGX5O2E7OD342#E\"}],\"r\":null}}"
-        val json = JSONObject(jsonString)
-
-        val infoScreen = infoScreenUtil.getForEuropeanTestQr(json)
-
-        assertEquals("Over mijn internationale QR-code", infoScreen.title)
-        assertEquals("Reis je buiten Nederland? Dan zijn er meer gegevens nodig. Daarom staan in de internationale QR-code de volgende gegevens:<br/><br/>Naam / Name:<br/><b>ten Bouwer, Bob</b><br/><br/>Geboortedatum / Date of birth*:<br/><b>18-08-1991</b><br/><br/>Ziekteverwekker / Disease targeted:<br/><b>COVID-19</b><br/><br/>Type test / Type of test:<br/><b>LP6464-4</b><br/><br/>Test naam / Test name:<br/><b></b><br/><br/>Testdatum / Test date*:<br/><b>10-08-2021</b><br/><br/>Testuitslag / Test result:<br/><b>negatief (geen corona)</b><br/><br/>Testlocatie / Testing centre:<br/><b>Facility approved by the State of The Netherlands</b><br/><br/>Producent / Test manufacturer:<br/><b></b><br/><br/>Getest in / Member state of test:<br/><b>Netherlands</b><br/><br/>Afgever certificaat / Certificate issuer:<br/><b>Ministerie van VWS / Ministry of Health, Welfare and Sport</b><br/><br/>Uniek certificaatnummer / Unique certificate identifier:<br/><b>URN:UCI:01:NL:PJ7JLSZ4KRGX5O2E7OD342#E</b><br/><br/>*Datum weergegeven in dag-maand-jaar / Date noted in day-month-year.", infoScreen.description)
-    }
-
 }
