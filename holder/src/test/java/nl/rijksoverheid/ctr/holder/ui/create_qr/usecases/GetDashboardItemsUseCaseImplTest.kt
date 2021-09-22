98f0870c81c68de761bdf7ce7254ff7745d784cf
Bart Nijland <bartnijland91@gmail.com>
2021-09-22 10:55:00 +0200

    Rewrite to use robolectric

--- holder/src/test/java/nl/rijksoverheid/ctr/holder/ui/create_qr/usecases/GetDashboardItemsUseCaseImplTest.kt
+++ holder/src/test/java/nl/rijksoverheid/ctr/holder/ui/create_qr/usecases/GetDashboardItemsUseCaseImplTest.kt
@@ -1,28 +1,23 @@
 package nl.rijksoverheid.ctr.holder.ui.create_qr.usecases
 
 import kotlinx.coroutines.runBlocking
-import nl.rijksoverheid.ctr.holder.fakeCredentialUtil
-import nl.rijksoverheid.ctr.holder.fakeDashboardItemUtil
-import nl.rijksoverheid.ctr.holder.fakeGreenCardUtil
-import nl.rijksoverheid.ctr.holder.fakeOriginUtil
 import nl.rijksoverheid.ctr.holder.persistence.database.DatabaseSyncerResult
 import nl.rijksoverheid.ctr.holder.ui.create_qr.models.DashboardItem
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.inject
+import org.robolectric.RobolectricTestRunner
 
-class GetDashboardItemsUseCaseImplTest {
+@RunWith(RobolectricTestRunner::class)
+class GetDashboardItemsUseCaseImplTest: AutoCloseKoinTest() {
+
+    private val usecase: GetDashboardItemsUseCase by inject()
 
     @Test
     fun `getItems returns correct models when no green cards`() = runBlocking {
-        val usecase = GetDashboardItemsUseCaseImpl(
-            greenCardUtil = fakeGreenCardUtil(),
-            credentialUtil = fakeCredentialUtil(),
-            originUtil = fakeOriginUtil(),
-            dashboardItemUtil = fakeDashboardItemUtil(
-                shouldShowPlaceholderItem = true
-            )
-        )
         val dashboardItems = usecase.getItems(
             allGreenCards = listOf(),
             databaseSyncerResult = DatabaseSyncerResult.Success,
