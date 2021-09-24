7503d8cbe807616263d1fd2deb7cad1300c5fb8e
Bart Nijland <bartnijland91@gmail.com>
2021-09-24 16:06:32 +0200

    Added missing disclaimer text

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/myoverview/QrCodePagerAdapter.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/myoverview/QrCodePagerAdapter.kt
@@ -9,6 +9,13 @@ import nl.rijksoverheid.ctr.holder.databinding.ViewQrCodeBinding
 import nl.rijksoverheid.ctr.holder.ui.myoverview.models.QrCodeData
 import nl.rijksoverheid.ctr.shared.utils.Accessibility.setAccessibilityFocus
 
+/*
+ *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *   SPDX-License-Identifier: EUPL-1.2
+ *
+ */
 class QrCodePagerAdapter: RecyclerView.Adapter<QrCodeViewHolder>() {
     private val qrCodeDataList: MutableList<QrCodeData> = mutableListOf()
 
