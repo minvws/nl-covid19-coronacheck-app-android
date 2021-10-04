a3f59cb0576f29b2e72942db94c3444a1d8b6545
Aiden Shi <aiden@aidenshi.nl>
2021-10-04 15:11:55 +0200

    test for over vaccinated

---
+++ holder/src/test/java/nl/rijksoverheid/ctr/holder/ui/myoverview/models/QrCodeDataTest.kt
@@ -0,0 +1,34 @@
+package nl.rijksoverheid.ctr.holder.ui.myoverview.models
+
+import io.mockk.mockk
+import org.junit.Assert.*
+import org.junit.Test
+
+class QrCodeDataTest {
+
+    @Test
+    fun `European vaccination is over vaccinated when highest dose exceeds total dose`() {
+        val vaccination1 = QrCodeData.European.Vaccination(
+            highestDose = "1",
+            ofTotalDoses = "2",
+            bitmap = mockk(),
+            readEuropeanCredential = mockk()
+        )
+        val vaccination2 = QrCodeData.European.Vaccination(
+            highestDose = "2",
+            ofTotalDoses = "2",
+            bitmap = mockk(),
+            readEuropeanCredential = mockk()
+        )
+        val vaccination3 = QrCodeData.European.Vaccination(
+            highestDose = "3",
+            ofTotalDoses = "2",
+            bitmap = mockk(),
+            readEuropeanCredential = mockk()
+        )
+
+        assertFalse(vaccination1.isOverVaccinated)
+        assertFalse(vaccination2.isOverVaccinated)
+        assertTrue(vaccination3.isOverVaccinated)
+    }
+}
