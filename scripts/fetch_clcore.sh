59a2e14238a2706d05e8fedbd5f0eb3e22a784e1
Giorgos Papadopoulos <giorgos@giorgos.nl>
2021-10-05 12:53:37 +0200

    update library

--- scripts/fetch_clcore.sh
+++ scripts/fetch_clcore.sh
@@ -2,9 +2,10 @@ set -eux
 cd ../
 git clone git@github.com:minvws/nl-covid19-coronacheck-mobile-core-private.git tmp-mobilecore
 cd tmp-mobilecore
+git checkout proof-identifier-denylist
 git submodule init
 git submodule update
-go get golang.org/x/mobile/cmd/gobind@latest
+go get -d golang.org/x/mobile/cmd/gobind@latest
 gomobile init
 gomobile bind -target android -o mobilecore.aar github.com/minvws/nl-covid19-coronacheck-mobile-core
 cd ../
