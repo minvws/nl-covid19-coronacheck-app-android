80184ff09a8335405dc9372621fb99985c9d0b9d
Bart Nijland <bartnijland91@gmail.com>
2021-10-11 16:44:03 +0200

    Add missing copyright text

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/util/EventGroupEntityUtil.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/util/EventGroupEntityUtil.kt
@@ -5,6 +5,13 @@ import nl.rijksoverheid.ctr.holder.persistence.database.entities.OriginType
 import nl.rijksoverheid.ctr.holder.ui.create_qr.models.RemoteConfigProviders
 import nl.rijksoverheid.ctr.holder.ui.create_qr.models.RemoteEventVaccination
 
+/*
+ *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *   SPDX-License-Identifier: EUPL-1.2
+ *
+ */
 interface EventGroupEntityUtil {
     suspend fun amountOfVaccinationEvents(eventGroupEntities: List<EventGroupEntity>): Int
 }
