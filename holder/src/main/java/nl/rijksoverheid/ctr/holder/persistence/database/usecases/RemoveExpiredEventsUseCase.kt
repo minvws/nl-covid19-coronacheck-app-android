5371e49058ec7f730ab733a092a3be6127beea00
Bart Nijland <bartnijland91@gmail.com>
2021-11-01 14:28:12 +0100

    Deprecate old config values

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/persistence/database/usecases/RemoveExpiredEventsUseCase.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/persistence/database/usecases/RemoveExpiredEventsUseCase.kt
@@ -6,6 +6,7 @@ import nl.rijksoverheid.ctr.holder.persistence.database.entities.EventGroupEntit
 import nl.rijksoverheid.ctr.holder.persistence.database.entities.OriginType
 import java.time.Clock
 import java.time.OffsetDateTime
+import java.util.concurrent.TimeUnit
 
 interface RemoveExpiredEventsUseCase {
     suspend fun execute(events: List<EventGroupEntity>)
@@ -20,19 +21,19 @@ class RemoveExpiredEventsUseCaseImpl(
     override suspend fun execute(events: List<EventGroupEntity>) {
         events.forEach {
             val cachedAppConfig = cachedAppConfigUseCase.getCachedAppConfig()
-            val expirationTime = when(it.type) {
+            val expirationTimeInHours = when(it.type) {
                 is OriginType.Vaccination -> {
-                    cachedAppConfig.vaccinationEventValidity
+                    TimeUnit.DAYS.toHours(cachedAppConfig.vaccinationEventValidityDays.toLong())
                 }
                 is OriginType.Test -> {
-                    cachedAppConfig.testEventValidity
+                    cachedAppConfig.testEventValidityHours.toLong()
                 }
                 is OriginType.Recovery -> {
-                    cachedAppConfig.recoveryEventValidity
+                    TimeUnit.DAYS.toHours(cachedAppConfig.recoveryEventValidityDays.toLong())
                 }
             }
 
-            if (it.maxIssuedAt.plusHours(expirationTime.toLong()) <= OffsetDateTime.now(clock)) {
+            if (it.maxIssuedAt.plusHours(expirationTimeInHours) <= OffsetDateTime.now(clock)) {
                 holderDatabase.eventGroupDao().delete(it)
             }
         }
