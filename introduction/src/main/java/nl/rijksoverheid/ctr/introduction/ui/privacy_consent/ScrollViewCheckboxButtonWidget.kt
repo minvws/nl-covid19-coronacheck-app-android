833f4b7321eff8fbd9182a9c965b185dee933a8f
Giorgos Papadopoulos <giorgos@giorgos.nl>
2021-11-23 16:27:28 +0100

    checkbox must be visible always and not have to scroll to see it

---
+++ introduction/src/main/java/nl/rijksoverheid/ctr/introduction/ui/privacy_consent/ScrollViewCheckboxButtonWidget.kt
@@ -0,0 +1,76 @@
+package nl.rijksoverheid.ctr.introduction.ui.privacy_consent
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.ScrollView
+import androidx.cardview.widget.CardView
+import nl.rijksoverheid.ctr.introduction.R
+import nl.rijksoverheid.ctr.introduction.databinding.WidgetScrollViewCheckboxButtonBinding
+
+
+/*
+ *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *   SPDX-License-Identifier: EUPL-1.2
+ *
+ */
+class ScrollViewCheckboxButtonWidget@JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : CardView(context, attrs, defStyleAttr) {
+
+    private val binding: WidgetScrollViewCheckboxButtonBinding
+
+    private var attachToScrollViewId: Int? = null
+    private var scrollViewGlobalLayoutListener: ViewTreeObserver.OnGlobalLayoutListener? = null
+
+    init {
+        elevation = 0f
+        WidgetScrollViewCheckboxButtonBinding.inflate(LayoutInflater.from(context), this)
+        binding = WidgetScrollViewCheckboxButtonBinding.bind(this)
+
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.ScrollViewCheckboxButtonWidget,
+            0, 0
+        ).apply {
+            try {
+                attachToScrollViewId =
+                    getResourceId(R.styleable.ScrollViewCheckboxButtonWidget_attachToScrollView, -1)
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        attachToScrollViewId?.let {
+            val parentLayout = parent as ViewGroup
+            val scrollView = parentLayout.findViewById<ScrollView>(it)
+            scrollViewGlobalLayoutListener = ViewTreeObserver.OnGlobalLayoutListener {
+                cardElevation = if (scrollView?.canScrollVertically(1) == true) {
+                    resources.getDimensionPixelSize(R.dimen.scroll_view_button_elevation)
+                        .toFloat()
+                } else {
+                    0f
+                }
+            }
+            scrollView.viewTreeObserver.addOnGlobalLayoutListener(scrollViewGlobalLayoutListener)
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+
+        attachToScrollViewId?.let {
+            val parentLayout = parent as ViewGroup
+            val scrollView = parentLayout.findViewById<ScrollView>(it)
+            scrollView.viewTreeObserver.removeOnGlobalLayoutListener(scrollViewGlobalLayoutListener)
+        }
+    }
+}
