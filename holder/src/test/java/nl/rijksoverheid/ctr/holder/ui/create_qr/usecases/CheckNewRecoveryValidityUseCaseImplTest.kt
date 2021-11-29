57a275871b7fbcd2481222b1c6b72b8bb120dba3
Giorgos Papadopoulos <giorgos@giorgos.nl>
2021-11-29 09:55:01 +0100

    merge conflict

--- holder/src/test/java/nl/rijksoverheid/ctr/holder/ui/create_qr/usecases/CheckNewRecoveryValidityUseCaseImplTest.kt
+++ holder/src/test/java/nl/rijksoverheid/ctr/holder/ui/create_qr/usecases/CheckNewRecoveryValidityUseCaseImplTest.kt
@@ -20,6 +20,13 @@ import java.time.Instant
 import java.time.OffsetDateTime
 import java.time.ZoneId
 
+/*
+ *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *   SPDX-License-Identifier: EUPL-1.2
+ *
+ */
 class CheckNewRecoveryValidityUseCaseImplTest {
     private val removeExpiredEventsUseCase = mockk<RemoveExpiredEventsUseCase>(relaxed = true)
 
