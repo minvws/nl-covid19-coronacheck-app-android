466d5ad6300e158dc8b1e346d6ccc31ba00a0a00
Sander van 't Veer <sander@egeniq.com>
2021-12-01 16:14:12 +0100

    Replaced copy import script for Lokalise transition

--- scripts/download_copy.py
+++ scripts/download_copy.py
@@ -1,30 +1,49 @@
-import requests
-import time
-import hashlib 
+import requests, zipfile
+from io import BytesIO
 import os
+import lokalise
 
-if os.getenv('CORONACHECK_ONESKY_SECRET') is not None and os.getenv('CORONACHECK_ONESKY_PUBLIC') is not None:
-	publickey = os.getenv('CORONACHECK_ONESKY_PUBLIC')
-	secretkey = os.getenv('CORONACHECK_ONESKY_SECRET')
-	timestamp = time.time()
-	devhash = hashlib.md5((str(timestamp) + secretkey).encode('utf-8')).hexdigest()
+if os.getenv('CORONACHECK_LOKALISE_TOKEN') is not None:
+	apikey = os.getenv('CORONACHECK_LOKALISE_TOKEN')
 
-	print("Downloading NL copy for Holder")
-	r = requests.get("https://platform.api.onesky.io/1/projects/380524/translations", params = {'api_key': publickey, 'timestamp': timestamp, 'dev_hash': devhash, 'locale': "nl", "source_file_name": "strings.xml", "export_file_name": "strings.xml"})
-	open('../holder/src/main/res/values/strings.xml', 'wb').write(r.content)
+	print("Initializing Lokalise client...")
+	client = lokalise.Client(apikey)
 
-	print("Downloading EN copy for Holder")
-	r = requests.get("https://platform.api.onesky.io/1/projects/380524/translations", params = {'api_key': publickey, 'timestamp': timestamp, 'dev_hash': devhash, 'locale': "en", "source_file_name": "strings.xml", "export_file_name": "strings.xml"})
-	open('../holder/src/main/res/values-en/strings.xml', 'wb').write(r.content)
+	# Download holder copy
+	print("Downloading holder copy...")
+	response = client.download_files('5229025261717f4fcb81c1.73606773', {
+		"format": "xml",
+		"original_filenames": True,
+		"replace_breaks": False,
+		"filter_langs" : ["en","nl"],
+		"export_sort" : "first_added"
+	})
+	req = requests.get(response['bundle_url'])
+	# Extract copy from zip
+	print("Extracting holder copy...")
+	zipHolder = zipfile.ZipFile(BytesIO(req.content))
+	zipHolder.extractall('../holder/src/main/res/')
+	print("Finished downloading holder copy!")
+	print()
 
-	print("Downloading NL copy for Verifier")
-	r = requests.get("https://platform.api.onesky.io/1/projects/380530/translations", params = {'api_key': publickey, 'timestamp': timestamp, 'dev_hash': devhash, 'locale': "nl", "source_file_name": "strings.xml", "export_file_name": "strings.xml"})
-	open('../verifier/src/main/res/values/strings.xml', 'wb').write(r.content)
+	# Download verifier copy
+	print("Downloading verifier copy...")
+	response = client.download_files('243601816196631318a279.00348152', {
+		"format": "xml",
+		"original_filenames": True,
+		"replace_breaks": False,
+		"filter_langs" : ["en","nl"],
+		"export_sort" : "first_added"
+	})
+	req = requests.get(response['bundle_url'])
+	# Extract copy from zip
+	print("Extracting verifier copy...")
+	zipVerifier = zipfile.ZipFile(BytesIO(req.content))
+	zipVerifier.extractall('../verifier/src/main/res/')
+	print("Finished verifier copy!")
+	print()
 
-	print("Downloading EN copy for Verifier")
-	r = requests.get("https://platform.api.onesky.io/1/projects/380530/translations", params = {'api_key': publickey, 'timestamp': timestamp, 'dev_hash': devhash, 'locale': "en", "source_file_name": "strings.xml", "export_file_name": "strings.xml"})
-	open('../verifier/src/main/res/values-en/strings.xml', 'wb').write(r.content)
-
-	print("Finished downloading copy")
+	print("All done!")
 else:
-	print("Please set CORONACHECK_ONESKY_SECRET and CORONACHECK_ONESKY_PUBLIC environment variable")
+	print("Please set CORONACHECK_LOKALISE_TOKEN")
+
