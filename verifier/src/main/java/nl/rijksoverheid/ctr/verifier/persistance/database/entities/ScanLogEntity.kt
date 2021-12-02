354162c5cff760f36d6a84bf04081b764c1c3f2d
Bart Nijland <bartnijland91@gmail.com>
2021-12-02 09:53:00 +0100

    Default primary key to 0

--- verifier/src/main/java/nl/rijksoverheid/ctr/verifier/persistance/database/entities/ScanLogEntity.kt
+++ verifier/src/main/java/nl/rijksoverheid/ctr/verifier/persistance/database/entities/ScanLogEntity.kt
@@ -16,7 +16,7 @@ import java.time.OffsetDateTime
     tableName = "scan_log",
 )
 data class ScanLogEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val policy: VerificationPolicy,
     val date: OffsetDateTime
 )
