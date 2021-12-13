0a6ae3240b818df2d8470d9809584321dc519866
Giorgos Papadopoulos <giorgos@giorgos.nl>
2021-12-13 13:56:04 +0100

    add missing tests

--- verifier/src/test/java/nl/rijksoverheid/ctr/verifier/ui/policy/VerificationPolicySelectionFragmentTest.kt
+++ verifier/src/test/java/nl/rijksoverheid/ctr/verifier/ui/policy/VerificationPolicySelectionFragmentTest.kt
@@ -8,17 +8,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.ViewMatchers.*
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotDisplayed
+import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotExist
 import com.adevinta.android.barista.interaction.BaristaClickInteractions.clickOn
-import io.mockk.*
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import nl.rijksoverheid.ctr.design.utils.DialogUtilImpl
 import nl.rijksoverheid.ctr.shared.livedata.Event
 import nl.rijksoverheid.ctr.shared.models.VerificationPolicy
 import nl.rijksoverheid.ctr.verifier.R
 import nl.rijksoverheid.ctr.verifier.models.ScannerState
 import nl.rijksoverheid.ctr.verifier.ui.scanner.utils.ScannerUtil
+import org.hamcrest.CoreMatchers.allOf
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -79,36 +88,88 @@ class VerificationPolicySelectionFragmentTest : AutoCloseKoinTest() {
     }
 
     @Test
-    fun `given default policy selection fragment and stored 2g selection, when confirming 3g selection, then confirmation dialog shows up`() {
+    fun `given default policy selection fragment and stored 2g selection, when confirming 3g selection, then correct subheader and confirmation dialog show up`() {
         launchFragment(
             policyState = VerificationPolicyState.Policy2G,
-            selectionType = VerificationPolicySelectionType.Default(ScannerState.Unlocked(VerificationPolicyState.Policy2G)),
+            selectionType = VerificationPolicySelectionType.Default(
+                ScannerState.Unlocked(
+                    VerificationPolicyState.Policy2G
+                )
+            ),
         )
 
         clickOn(R.id.policy3G_container)
         clickOn(R.id.confirmationButton)
+        clickOnDialogPositiveButton(R.string.verifier_risksetting_confirmation_dialog_positive_button)
+
+        assertNotExist(
+            context.getString(
+                R.string.verifier_risksetting_menu_scan_settings_unselected_title,
+            )
+        )
+        assertEquals(navController.currentDestination?.id, R.id.nav_scan_qr)
+    }
+
+    private fun clickOnDialogPositiveButton(positiveButtonTextStringId: Int) {
+        onView(allOf(withId(android.R.id.button1), withText(positiveButtonTextStringId)))
+            .inRoot(isDialog())
+            .check(matches(isDisplayed()))
+            .perform(ViewActions.click())
     }
 
     @Test
     fun `given default policy selection fragment and stored 2g selection, when confirming 2g selection, then goes to home screen`() {
+        launchFragment(
+            policyState = VerificationPolicyState.Policy2G,
+            selectionType = VerificationPolicySelectionType.Default(
+                ScannerState.Unlocked(
+                    VerificationPolicyState.Policy2G
+                )
+            ),
+        )
 
+        clickOn(R.id.policy2G_container)
+        clickOn(R.id.confirmationButton)
+
+        assertEquals(navController.currentDestination?.id, R.id.nav_scan_qr)
+    }
+
+    @Test
+    fun `given default policy selection fragment and scan not used recently, then correct subheader is showing`() {
+        launchFragment(
+            policyState = VerificationPolicyState.Policy2G,
+            selectionType = VerificationPolicySelectionType.Default(
+                ScannerState.Unlocked(
+                    VerificationPolicyState.Policy2G
+                )
+            ),
+            scannerUsedRecently = false,
+        )
+
+        assertDisplayed(
+            context.getString(R.string.verifier_risksetting_menu_scan_settings_unselected_title)
+        )
     }
 
     private fun launchFragment(
         policyState: VerificationPolicyState = VerificationPolicyState.None,
-        selectionType: VerificationPolicySelectionType = VerificationPolicySelectionType.FirstTimeUse(ScannerState.Unlocked(policyState)),
+        selectionType: VerificationPolicySelectionType = VerificationPolicySelectionType.FirstTimeUse(
+            ScannerState.Unlocked(policyState)
+        ),
+        scannerUsedRecently: Boolean = true,
     ) {
 
-        val verificationPolicyStateUseCase = mockk<VerificationPolicyStateUseCase>(relaxed = true).apply {
-            every { get() } returns policyState
-        }
+        val verificationPolicyStateUseCase =
+            mockk<VerificationPolicyStateUseCase>(relaxed = true).apply {
+                every { get() } returns policyState
+            }
 
         val recentScanLogsLiveDataEvent = MutableLiveData<Event<Boolean>>()
 
-        verificationPolicySelectionViewModel =  mockk<VerificationPolicySelectionViewModel>().apply {
+        verificationPolicySelectionViewModel = mockk<VerificationPolicySelectionViewModel>().apply {
             coEvery { scannerUsedRecentlyLiveData } returns recentScanLogsLiveDataEvent
-            coEvery { didScanRecently() }  answers {
-                recentScanLogsLiveDataEvent.postValue(Event(true))
+            coEvery { didScanRecently() } answers {
+                recentScanLogsLiveDataEvent.postValue(Event(scannerUsedRecently))
             }
             coEvery { radioButtonSelected } returns when (policyState) {
                 VerificationPolicyState.None -> null
