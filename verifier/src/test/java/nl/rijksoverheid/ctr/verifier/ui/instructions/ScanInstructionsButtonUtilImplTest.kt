792d0f50cf864d6500da49eaa4e893bae4b9e8ad
Giorgos Papadopoulos <giorgos@giorgos.nl>
2021-12-14 09:00:50 +0100

    fix copy for button last page instructions

--- verifier/src/test/java/nl/rijksoverheid/ctr/verifier/ui/instructions/ScanInstructionsButtonUtilImplTest.kt
+++ verifier/src/test/java/nl/rijksoverheid/ctr/verifier/ui/instructions/ScanInstructionsButtonUtilImplTest.kt
@@ -3,50 +3,48 @@ package nl.rijksoverheid.ctr.verifier.ui.instructions
 import io.mockk.every
 import io.mockk.mockk
 import nl.rijksoverheid.ctr.verifier.R
-import nl.rijksoverheid.ctr.verifier.ui.scanqr.ScannerNavigationState
-import nl.rijksoverheid.ctr.verifier.ui.scanqr.ScannerNavigationStateUseCase
-import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Test
 
 class ScanInstructionsButtonUtilImplTest {
 
     @Test
     fun `getButtonText returns correct text when last screen and scanner locked`() {
-        val scannerNavigationStateUseCase = mockk<ScannerNavigationStateUseCase>()
-        every { scannerNavigationStateUseCase.get() } answers { ScannerNavigationState.Scanner(isLocked = true) }
-        val util = ScanInstructionsButtonUtilImpl(scannerNavigationStateUseCase)
+        val instructionsNavigateStateUseCase = mockk<InstructionsNavigateStateUseCase>()
+        every { instructionsNavigateStateUseCase.get() } answers { InstructionsNavigateState.Scanner(isLocked = true) }
+        val util = ScanInstructionsButtonUtilImpl(instructionsNavigateStateUseCase)
 
         val buttonText = util.getButtonText(true)
-        Assert.assertEquals(buttonText, R.string.verifier_scan_instructions_back_to_start)
+        assertEquals(buttonText, R.string.verifier_scan_instructions_back_to_start)
     }
 
     @Test
     fun `getButtonText returns correct text when last screen and scanner not locked`() {
-        val scannerNavigationStateUseCase = mockk<ScannerNavigationStateUseCase>()
-        every { scannerNavigationStateUseCase.get() } answers { ScannerNavigationState.Scanner(isLocked = false) }
-        val util = ScanInstructionsButtonUtilImpl(scannerNavigationStateUseCase)
+        val instructionsNavigateStateUseCase = mockk<InstructionsNavigateStateUseCase>()
+        every { instructionsNavigateStateUseCase.get() } answers { InstructionsNavigateState.Scanner(isLocked = false) }
+        val util = ScanInstructionsButtonUtilImpl(instructionsNavigateStateUseCase)
 
         val buttonText = util.getButtonText(true)
-        Assert.assertEquals(buttonText, R.string.scan_qr_button)
-    }
-
-    @Test
-    fun `getButtonText returns correct text when last screen and next screen is not scanner`() {
-        val scannerNavigationStateUseCase = mockk<ScannerNavigationStateUseCase>()
-        every { scannerNavigationStateUseCase.get() } answers { ScannerNavigationState.Instructions }
-        val util = ScanInstructionsButtonUtilImpl(scannerNavigationStateUseCase)
-
-        val buttonText = util.getButtonText(true)
-        Assert.assertEquals(buttonText, R.string.onboarding_next)
+        assertEquals(buttonText, R.string.scan_qr_button)
     }
 
     @Test
     fun `getButtonText returns correct text when not last screen`() {
-        val scannerNavigationStateUseCase = mockk<ScannerNavigationStateUseCase>()
-        every { scannerNavigationStateUseCase.get() } answers { ScannerNavigationState.Scanner(isLocked = false) }
-        val util = ScanInstructionsButtonUtilImpl(scannerNavigationStateUseCase)
+        val instructionsNavigateStateUseCase = mockk<InstructionsNavigateStateUseCase>()
+        every { instructionsNavigateStateUseCase.get() } answers { InstructionsNavigateState.Scanner(isLocked = false) }
+        val util = ScanInstructionsButtonUtilImpl(instructionsNavigateStateUseCase)
 
         val buttonText = util.getButtonText(false)
-        Assert.assertEquals(buttonText, R.string.onboarding_next)
+        assertEquals(buttonText, R.string.onboarding_next)
+    }
+
+    @Test
+    fun `getButtonText returns onboarding_next when last screen and policy not set yet`() {
+        val instructionsNavigateStateUseCase = mockk<InstructionsNavigateStateUseCase>()
+        every { instructionsNavigateStateUseCase.get() } answers { InstructionsNavigateState.VerificationPolicySelection }
+        val util = ScanInstructionsButtonUtilImpl(instructionsNavigateStateUseCase)
+
+        val buttonText = util.getButtonText(true)
+        assertEquals(buttonText, R.string.onboarding_next)
     }
 }
