6d99684305eb5504eba2205b58dc473b86d80084
Aiden Shi <aiden@aidenshi.nl>
2022-01-05 15:05:53 +0100

    can't create qr dialog add holder flow bes islands

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/digid/MijnCnFragment.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/digid/MijnCnFragment.kt
@@ -17,15 +17,18 @@ import nl.rijksoverheid.ctr.holder.R
 import nl.rijksoverheid.ctr.holder.databinding.FragmentMijnCnBinding
 import nl.rijksoverheid.ctr.holder.launchUrl
 import nl.rijksoverheid.ctr.holder.persistence.database.entities.OriginType
-import nl.rijksoverheid.ctr.holder.ui.create_qr.*
+import nl.rijksoverheid.ctr.holder.ui.create_qr.GetEventsFragmentCopy
+import nl.rijksoverheid.ctr.holder.ui.create_qr.GetEventsViewModel
+import nl.rijksoverheid.ctr.holder.ui.create_qr.YourEventsFragmentType
+import nl.rijksoverheid.ctr.holder.ui.create_qr.bind
 import nl.rijksoverheid.ctr.holder.ui.create_qr.models.*
+import nl.rijksoverheid.ctr.shared.ext.findNavControllerSafety
 import nl.rijksoverheid.ctr.shared.ext.navigateSafety
 import nl.rijksoverheid.ctr.shared.livedata.EventObserver
 import nl.rijksoverheid.ctr.shared.models.ErrorResultFragmentData
 import nl.rijksoverheid.ctr.shared.models.Flow
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import timber.log.Timber
 
 class MijnCnFragment : DigiDFragment(R.layout.fragment_mijn_cn) {
 
@@ -36,7 +39,7 @@ class MijnCnFragment : DigiDFragment(R.layout.fragment_mijn_cn) {
     }
 
     override fun getFlow(): Flow {
-        return HolderFlow.Vaccination
+        return HolderFlow.VaccinationBesIslands
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -108,7 +111,6 @@ class MijnCnFragment : DigiDFragment(R.layout.fragment_mijn_cn) {
                             }
                         )
                     } else {
-                        Timber.d("We got events!")
                         navigateToYourEvents(
                             signedEvents = it.signedModels,
                             eventProviders = it.eventProviders,
@@ -120,7 +122,10 @@ class MijnCnFragment : DigiDFragment(R.layout.fragment_mijn_cn) {
                         presentError(
                             data = ErrorResultFragmentData(
                                 title = getString(R.string.error_get_events_no_events_title),
-                                description = getString(R.string.error_get_events_http_error_description, getErrorCodes(it.errorResults)),
+                                description = getString(
+                                    R.string.error_get_events_http_error_description,
+                                    getErrorCodes(it.errorResults)
+                                ),
                                 buttonTitle = getString(R.string.back_to_overview),
                                 ErrorResultFragmentData.ButtonAction.Destination(R.id.action_my_overview),
                                 urlData = ErrorResultFragmentData.UrlData(
@@ -130,7 +135,14 @@ class MijnCnFragment : DigiDFragment(R.layout.fragment_mijn_cn) {
                             )
                         )
                     } else {
-                       Timber.d("MEssed up missingevents")
+                        findNavControllerSafety()?.navigate(
+                            MijnCnFragmentDirections.actionCouldNotCreateQr(
+                                toolbarTitle = getString(R.string.your_vaccination_result_toolbar_title),
+                                title = getString(R.string.no_vaccinations_title),
+                                description = getString(R.string.no_vaccinations_description),
+                                buttonTitle = getString(R.string.back_to_overview)
+                            )
+                        )
                     }
                 }
                 is EventsResult.Error -> {
@@ -153,9 +165,14 @@ class MijnCnFragment : DigiDFragment(R.layout.fragment_mijn_cn) {
                             presentError(
                                 data = ErrorResultFragmentData(
                                     title = getString(R.string.event_provider_error_title),
-                                    description = getString(R.string.mijncn_error_missing_data_description, errorCodeString),
+                                    description = getString(
+                                        R.string.mijncn_error_missing_data_description,
+                                        errorCodeString
+                                    ),
                                     buttonTitle = getString(R.string.back_to_overview),
-                                    buttonAction = ErrorResultFragmentData.ButtonAction.Destination(R.id.action_my_overview)
+                                    buttonAction = ErrorResultFragmentData.ButtonAction.Destination(
+                                        R.id.action_my_overview
+                                    )
                                 )
                             )
                         }
