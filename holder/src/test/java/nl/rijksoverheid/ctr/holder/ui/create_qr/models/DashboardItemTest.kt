14c61837719d2bca843b4024aa4eae86462f5b7a
Aiden Shi <aiden@aidenshi.nl>
2022-01-13 12:26:14 +0100

    update dashboard item test

--- holder/src/test/java/nl/rijksoverheid/ctr/holder/ui/create_qr/models/DashboardItemTest.kt
+++ holder/src/test/java/nl/rijksoverheid/ctr/holder/ui/create_qr/models/DashboardItemTest.kt
@@ -20,16 +20,42 @@ class DashboardItemTest {
 
     @Test
     fun `Green card expired item has a read more button when it's a domestic vaccination`() {
-        val euRecoveryGreenCard = fakeGreenCard(
-            greenCardType = GreenCardType.Eu,
-            originType = OriginType.Recovery
-        )
         val domesticVaccinationGreenCard = fakeGreenCard(
             greenCardType = GreenCardType.Domestic,
             originType = OriginType.Vaccination
         )
 
-        assertFalse(DashboardItem.InfoItem.GreenCardExpiredItem(euRecoveryGreenCard).hasButton)
         assertTrue(DashboardItem.InfoItem.GreenCardExpiredItem(domesticVaccinationGreenCard).hasButton)
+    }
+
+    @Test
+    fun `Green card expired item has no read more button when it's not a domestic vaccination`() {
+        val euRecoveryGreenCard = fakeGreenCard(
+            greenCardType = GreenCardType.Eu,
+            originType = OriginType.Recovery
+        )
+        val domesticRecoveryGreenCard = fakeGreenCard(
+            greenCardType = GreenCardType.Domestic,
+            originType = OriginType.Recovery
+        )
+        val euVaccinationGreenCard = fakeGreenCard(
+            greenCardType = GreenCardType.Eu,
+            originType = OriginType.Vaccination
+        )
+        val euTestGreenCard = fakeGreenCard(
+            greenCardType = GreenCardType.Eu,
+            originType = OriginType.Test
+        )
+        val domesticTestGreenCard = fakeGreenCard(
+            greenCardType = GreenCardType.Domestic,
+            originType = OriginType.Test
+        )
+
+
+        assertFalse(DashboardItem.InfoItem.GreenCardExpiredItem(euRecoveryGreenCard).hasButton)
+        assertFalse(DashboardItem.InfoItem.GreenCardExpiredItem(domesticRecoveryGreenCard).hasButton)
+        assertFalse(DashboardItem.InfoItem.GreenCardExpiredItem(euVaccinationGreenCard).hasButton)
+        assertFalse(DashboardItem.InfoItem.GreenCardExpiredItem(euTestGreenCard).hasButton)
+        assertFalse(DashboardItem.InfoItem.GreenCardExpiredItem(domesticTestGreenCard).hasButton)
     }
 }
