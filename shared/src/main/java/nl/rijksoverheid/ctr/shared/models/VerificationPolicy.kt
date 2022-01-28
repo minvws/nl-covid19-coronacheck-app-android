3efa35ad37096c9cc249184d6089278a59a28f2d
Aiden Shi <aiden@aidenshi.nl>
2022-01-28 12:31:19 +0100

    replace with library constant

--- shared/src/main/java/nl/rijksoverheid/ctr/shared/models/VerificationPolicy.kt
+++ shared/src/main/java/nl/rijksoverheid/ctr/shared/models/VerificationPolicy.kt
@@ -11,13 +11,12 @@ import mobilecore.Mobilecore
  */
 sealed class VerificationPolicy(val libraryValue: String, val configValue: String) {
     object VerificationPolicy3G : VerificationPolicy(Mobilecore.VERIFICATION_POLICY_3G, "3G")
-    // TODO replace "5" with library static value when ready (task 3039)
-    object VerificationPolicy1G : VerificationPolicy("5", "1G")
+    object VerificationPolicy1G : VerificationPolicy(Mobilecore.VERIFICATION_POLICY_1G, "1G")
 
     companion object {
         fun fromString(value: String?): VerificationPolicy? = when (value) {
             Mobilecore.VERIFICATION_POLICY_3G -> VerificationPolicy3G
-            "5" -> VerificationPolicy1G
+            Mobilecore.VERIFICATION_POLICY_1G -> VerificationPolicy1G
             else -> null
         }
     }
