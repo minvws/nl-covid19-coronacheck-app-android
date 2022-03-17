cd4e57ec297f65a9e0b45c65d7035e0478624eed
Aiden Shi <aiden@aidenshi.nl>
2022-03-17 10:49:20 +0100

    different link texts dashboard header 0G on empty state

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/dashboard/items/DashboardHeaderAdapterItemUtil.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/dashboard/items/DashboardHeaderAdapterItemUtil.kt
@@ -9,20 +9,21 @@ package nl.rijksoverheid.ctr.holder.dashboard.items
 
 import nl.rijksoverheid.ctr.holder.R
 import nl.rijksoverheid.ctr.holder.persistence.database.entities.GreenCardType
+import nl.rijksoverheid.ctr.holder.ui.create_qr.models.DashboardItem
 import nl.rijksoverheid.ctr.holder.usecase.HolderFeatureFlagUseCase
 import nl.rijksoverheid.ctr.shared.models.DisclosurePolicy
 
 interface DashboardHeaderAdapterItemUtil {
-    fun getText(
+    fun getHeaderItem(
         greenCardType: GreenCardType,
         emptyState: Boolean,
         hasVisitorPassIncompleteItem: Boolean
-        ): Int
+    ): DashboardItem.HeaderItem
 }
 
 class DashboardHeaderAdapterItemUtilImpl(
     private val featureFlagUseCase: HolderFeatureFlagUseCase
-): DashboardHeaderAdapterItemUtil {
+) : DashboardHeaderAdapterItemUtil {
 
     /**
      * Get the header item text to display in the domestic tab on the dashboard screen
@@ -30,61 +31,84 @@ class DashboardHeaderAdapterItemUtilImpl(
      * @param emptyState If we should treat the dashboard as being empty state
      * @param hasVisitorPassIncompleteItem If there is a incomplete visitor pass item currently showing on the dashboard
      */
-    override fun getText(
+    override fun getHeaderItem(
         tabType: GreenCardType,
         emptyState: Boolean,
-        hasVisitorPassIncompleteItem: Boolean): Int {
+        hasVisitorPassIncompleteItem: Boolean
+    ): DashboardItem.HeaderItem {
+        val empty = emptyState || hasVisitorPassIncompleteItem
+        val text = getHeaderText(tabType, empty)
+        val buttonInfo = getButtonInfo(tabType, empty)
+        return DashboardItem.HeaderItem(text, buttonInfo)
+    }
 
-        when (tabType) {
-            is GreenCardType.Domestic -> {
-                val emptyState = emptyState || hasVisitorPassIncompleteItem
-
-                return when (featureFlagUseCase.getDisclosurePolicy()) {
-                    is DisclosurePolicy.OneG -> {
-                        if (emptyState) {
-                            R.string.holder_dashboard_empty_domestic_only1Gaccess_message
-                        } else {
-                            R.string.holder_dashboard_intro_domestic_only1Gaccess
-                        }
+    private fun getHeaderText(
+        tabType: GreenCardType,
+        emptyState: Boolean,
+    ) = when (tabType) {
+        is GreenCardType.Domestic -> {
+            when (featureFlagUseCase.getDisclosurePolicy()) {
+                is DisclosurePolicy.OneG -> {
+                    if (emptyState) {
+                        R.string.holder_dashboard_empty_domestic_only1Gaccess_message
+                    } else {
+                        R.string.holder_dashboard_intro_domestic_only1Gaccess
                     }
-                    is DisclosurePolicy.OneAndThreeG -> {
-                        if (emptyState) {
-                            R.string.holder_dashboard_empty_domestic_3Gand1Gaccess_message
-                        } else {
-                            R.string.holder_dashboard_intro_domestic_3Gand1Gaccess
-                        }
-                    }
-                    is DisclosurePolicy.ThreeG -> {
-                        if (emptyState) {
-                            R.string.my_overview_qr_placeholder_description
-                        } else {
-                            R.string.my_overview_description
-                        }
-                    }
-                    is DisclosurePolicy.ZeroG -> R.string.app_name // TODO: implement 0G
                 }
-            }
-            is GreenCardType.Eu -> {
-                return if (emptyState) {
-                    when (featureFlagUseCase.getDisclosurePolicy()) {
-                        is DisclosurePolicy.ZeroG -> {
-                            R.string.holder_dashboard_emptyState_international_0G_message
-                        }
-                        else -> {
-                            R.string.my_overview_qr_placeholder_description_eu
-                        }
+                is DisclosurePolicy.OneAndThreeG -> {
+                    if (emptyState) {
+                        R.string.holder_dashboard_empty_domestic_3Gand1Gaccess_message
+                    } else {
+                        R.string.holder_dashboard_intro_domestic_3Gand1Gaccess
                     }
-                } else {
-                    when (featureFlagUseCase.getDisclosurePolicy()) {
-                        is DisclosurePolicy.ZeroG -> {
-                            R.string.holder_dashboard_filledState_international_0G_message
-                        }
-                        else -> {
-                            R.string.my_overview_description_eu
-                        }
+                }
+                is DisclosurePolicy.ThreeG -> {
+                    if (emptyState) {
+                        R.string.my_overview_qr_placeholder_description
+                    } else {
+                        R.string.my_overview_description
+                    }
+                }
+                is DisclosurePolicy.ZeroG -> R.string.app_name // Not applicable
+            }
+        }
+        is GreenCardType.Eu -> {
+            if (emptyState) {
+                when (featureFlagUseCase.getDisclosurePolicy()) {
+                    is DisclosurePolicy.ZeroG -> {
+                        R.string.holder_dashboard_emptyState_international_0G_message
+                    }
+                    else -> {
+                        R.string.my_overview_qr_placeholder_description_eu
+                    }
+                }
+            } else {
+                when (featureFlagUseCase.getDisclosurePolicy()) {
+                    is DisclosurePolicy.ZeroG -> {
+                        R.string.holder_dashboard_filledState_international_0G_message
+                    }
+                    else -> {
+                        R.string.my_overview_description_eu
                     }
                 }
             }
         }
     }
+
+    private fun getButtonInfo(tabType: GreenCardType, empty: Boolean) =
+        if (tabType == GreenCardType.Eu) {
+            if (featureFlagUseCase.getDisclosurePolicy() is DisclosurePolicy.ZeroG && !empty) {
+                ButtonInfo(
+                    R.string.holder_dashboard_international_0G_action_certificateNeeded,
+                    R.string.my_overview_description_eu_button_link
+                )
+            } else {
+                ButtonInfo(
+                    R.string.my_overview_description_eu_button_text,
+                    R.string.my_overview_description_eu_button_link
+                )
+            }
+        } else {
+            null
+        }
 }
--- holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/models/DashboardItem.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/ui/create_qr/models/DashboardItem.kt
@@ -2,8 +2,12 @@ package nl.rijksoverheid.ctr.holder.ui.create_qr.models
 
 import androidx.annotation.StringRes
 import nl.rijksoverheid.ctr.holder.R
+import nl.rijksoverheid.ctr.holder.dashboard.items.ButtonInfo
 import nl.rijksoverheid.ctr.holder.persistence.database.DatabaseSyncerResult
-import nl.rijksoverheid.ctr.holder.persistence.database.entities.*
+import nl.rijksoverheid.ctr.holder.persistence.database.entities.CredentialEntity
+import nl.rijksoverheid.ctr.holder.persistence.database.entities.GreenCardType
+import nl.rijksoverheid.ctr.holder.persistence.database.entities.OriginEntity
+import nl.rijksoverheid.ctr.holder.persistence.database.entities.OriginType
 import nl.rijksoverheid.ctr.holder.persistence.database.models.GreenCard
 import nl.rijksoverheid.ctr.holder.ui.create_qr.util.OriginState
 import nl.rijksoverheid.ctr.shared.models.DisclosurePolicy
@@ -11,7 +15,10 @@ import nl.rijksoverheid.ctr.shared.models.GreenCardDisclosurePolicy
 
 sealed class DashboardItem {
 
-    data class HeaderItem(@StringRes val text: Int) : DashboardItem()
+    data class HeaderItem(
+        @StringRes val text: Int,
+        val buttonInfo: ButtonInfo?
+    ) : DashboardItem()
 
     data class PlaceholderCardItem(val greenCardType: GreenCardType) : DashboardItem()
 
