78f80481dfeff2cf56147747700e5cfe63c0f302
Aiden Shi <aiden@aidenshi.nl>
2022-04-04 11:02:10 +0200

    disable test class

--- holder/src/test/java/nl/rijksoverheid/ctr/HolderMainActivityTest.kt
+++ holder/src/test/java/nl/rijksoverheid/ctr/HolderMainActivityTest.kt
@@ -1,56 +1,56 @@
-/*
- * Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
- * Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
- *
- * SPDX-License-Identifier: EUPL-1.2
- */
-
-package nl.rijksoverheid.ctr
-
-import android.content.Context
-import android.content.Intent
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.navigation.findNavController
-import androidx.room.Room
-import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
-import nl.rijksoverheid.ctr.appconfig.models.AppStatus
-import nl.rijksoverheid.ctr.appconfig.models.AppUpdateData
-import nl.rijksoverheid.ctr.appconfig.models.NewTerms
-import nl.rijksoverheid.ctr.holder.*
-import nl.rijksoverheid.ctr.persistence.database.HolderDatabase
-import nl.rijksoverheid.ctr.introduction.IntroductionViewModel
-import org.junit.After
-import org.junit.Assert.assertEquals
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.context.loadKoinModules
-import org.koin.dsl.module
-import org.koin.test.AutoCloseKoinTest
-import org.robolectric.RobolectricTestRunner
-
-/*
- *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
- *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
- *
- *   SPDX-License-Identifier: EUPL-1.2
- *
- */
-@RunWith(RobolectricTestRunner::class)
-class HolderMainActivityTest : AutoCloseKoinTest() {
-
-    @get:Rule
-    val rule = InstantTaskExecutorRule()
-
-    private lateinit var scenario: ActivityScenario<HolderMainActivity>
-
-    @After
-    fun tearDown() {
-        scenario.close()
-    }
-
+///*
+// * Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// * Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// *
+// * SPDX-License-Identifier: EUPL-1.2
+// */
+//
+//package nl.rijksoverheid.ctr
+//
+//import android.content.Context
+//import android.content.Intent
+//import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+//import androidx.navigation.findNavController
+//import androidx.room.Room
+//import androidx.test.core.app.ActivityScenario
+//import androidx.test.core.app.ApplicationProvider
+//import nl.rijksoverheid.ctr.appconfig.models.AppStatus
+//import nl.rijksoverheid.ctr.appconfig.models.AppUpdateData
+//import nl.rijksoverheid.ctr.appconfig.models.NewTerms
+//import nl.rijksoverheid.ctr.holder.*
+//import nl.rijksoverheid.ctr.persistence.database.HolderDatabase
+//import nl.rijksoverheid.ctr.introduction.IntroductionViewModel
+//import org.junit.After
+//import org.junit.Assert.assertEquals
+//import org.junit.Rule
+//import org.junit.Test
+//import org.junit.runner.RunWith
+//import org.koin.androidx.viewmodel.dsl.viewModel
+//import org.koin.core.context.loadKoinModules
+//import org.koin.dsl.module
+//import org.koin.test.AutoCloseKoinTest
+//import org.robolectric.RobolectricTestRunner
+//
+///*
+// *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// *   Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// *
+// *   SPDX-License-Identifier: EUPL-1.2
+// *
+// */
+//@RunWith(RobolectricTestRunner::class)
+//class HolderMainActivityTest : AutoCloseKoinTest() {
+//
+//    @get:Rule
+//    val rule = InstantTaskExecutorRule()
+//
+//    private lateinit var scenario: ActivityScenario<HolderMainActivity>
+//
+//    @After
+//    fun tearDown() {
+//        scenario.close()
+//    }
+//
 //    @Test
 //    fun `If onboarding not finished navigate to introduction`() {
 //        launchHolderMainActivity(
@@ -123,43 +123,43 @@ class HolderMainActivityTest : AutoCloseKoinTest() {
 //            )
 //        }
 //    }
-
-    private fun launchHolderMainActivity(
-        introductionViewModel: IntroductionViewModel,
-        appStatus: AppStatus = AppStatus.NoActionRequired
-    ): ActivityScenario<HolderMainActivity> {
-
-        loadKoinModules(
-            module(override = true) {
-                viewModel {
-                    introductionViewModel
-                }
-                viewModel {
-                    fakeAppConfigViewModel(
-                        appStatus = appStatus
-                    )
-                }
-                viewModel {
-                    fakeDashboardViewModel()
-                }
-                factory {
-                    fakeSecretKeyUseCase()
-                }
-                factory {
-                    fakeCachedAppConfigUseCase()
-                }
-                single {
-                    val context = ApplicationProvider.getApplicationContext<Context>()
-                    Room.inMemoryDatabaseBuilder(context, HolderDatabase::class.java).build()
-                }
-            })
-
-        scenario = ActivityScenario.launch(
-            Intent(
-                ApplicationProvider.getApplicationContext(),
-                HolderMainActivity::class.java
-            )
-        )
-        return scenario
-    }
-}
+//
+//    private fun launchHolderMainActivity(
+//        introductionViewModel: IntroductionViewModel,
+//        appStatus: AppStatus = AppStatus.NoActionRequired
+//    ): ActivityScenario<HolderMainActivity> {
+//
+//        loadKoinModules(
+//            module(override = true) {
+//                viewModel {
+//                    introductionViewModel
+//                }
+//                viewModel {
+//                    fakeAppConfigViewModel(
+//                        appStatus = appStatus
+//                    )
+//                }
+//                viewModel {
+//                    fakeDashboardViewModel()
+//                }
+//                factory {
+//                    fakeSecretKeyUseCase()
+//                }
+//                factory {
+//                    fakeCachedAppConfigUseCase()
+//                }
+//                single {
+//                    val context = ApplicationProvider.getApplicationContext<Context>()
+//                    Room.inMemoryDatabaseBuilder(context, HolderDatabase::class.java).build()
+//                }
+//            })
+//
+//        scenario = ActivityScenario.launch(
+//            Intent(
+//                ApplicationProvider.getApplicationContext(),
+//                HolderMainActivity::class.java
+//            )
+//        )
+//        return scenario
+//    }
+//}
