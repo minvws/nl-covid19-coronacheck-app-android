e3f116b2b57ab7135a063867ebaa70e4cc926b23
Giorgos Papadopoulos <giorgos@giorgos.nl>
2022-04-06 23:25:03 +0200

    fix config almost out of date logic

--- appconfig/src/main/java/nl/rijksoverheid/ctr/appconfig/usecases/AppConfigFreshnessUseCase.kt
+++ appconfig/src/main/java/nl/rijksoverheid/ctr/appconfig/usecases/AppConfigFreshnessUseCase.kt
@@ -40,9 +40,14 @@ class AppConfigFreshnessUseCaseImpl(
     override fun shouldShowConfigFreshnessWarning(): Boolean {
         // return true if config is older than 10 days && less than 28 days, or when the configTTL has expired
         val lastFetched = getAppConfigLastFetchedSeconds()
-        return lastFetched > 0 && lastFetched < OffsetDateTime.now(clock)
-            .minusSeconds(cachedAppConfigUseCase.getCachedAppConfig().configAlmostOutOfDateWarningSeconds.toLong())
-            .toEpochSecond()
+        val now = OffsetDateTime.now(clock)
+        val config = cachedAppConfigUseCase.getCachedAppConfig()
+        val configNotExpiredYet =
+            lastFetched > now.minusSeconds(config.configTtlSeconds.toLong()).toEpochSecond()
+        val configAlmostExpired = lastFetched <= now.minusSeconds(config.configTtlSeconds.toLong())
+            .plusSeconds(config.configAlmostOutOfDateWarningSeconds.toLong()).toEpochSecond()
+        return lastFetched > 0 && configNotExpiredYet && configAlmostExpired
+
     }
 
 }
