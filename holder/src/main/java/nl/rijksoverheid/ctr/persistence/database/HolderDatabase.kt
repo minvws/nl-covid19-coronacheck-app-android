888681e2001b3d04e84b918285c7b2357db8f283
Aiden Shi <aiden@aidenshi.nl>
2022-04-15 18:21:59 +0200

    migration for event group table to set scope not null

--- holder/src/main/java/nl/rijksoverheid/ctr/persistence/database/HolderDatabase.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/persistence/database/HolderDatabase.kt
@@ -39,9 +39,32 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
     }
 }
 
+val MIGRATION_4_5 = object : Migration(4, 5) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        // create new table for event group with scope not null
+        database.execSQL("CREATE TABLE IF NOT EXISTS event_group_temp (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, wallet_id INTEGER NOT NULL, provider_identifier TEXT NOT NULL, type TEXT NOT NULL, scope TEXT NOT NULL, maxIssuedAt INTEGER NOT NULL, jsonData BLOB NOT NULL, FOREIGN KEY(wallet_id) REFERENCES wallet(id) ON UPDATE NO ACTION ON DELETE CASCADE )")
+
+        // update existing null scope values to empty strings
+        database.execSQL("UPDATE event_group SET scope = '' WHERE scope IS NULL")
+
+        // copy data from old event group table to new one
+        database.execSQL("INSERT INTO event_group_temp (id, wallet_id, provider_identifier, type, scope, maxIssuedAt, jsonData) SELECT id, wallet_id, provider_identifier, type, scope, maxIssuedAt, jsonData FROM event_group")
+
+        // delete old event group table and index
+        database.execSQL("DROP TABLE IF EXISTS event_group")
+        database.execSQL("DROP INDEX IF EXISTS index_event_group_provider_identifier_type_scope")
+        database.execSQL("DROP INDEX IF EXISTS index_event_group_wallet_id")
+
+        // rename new table and set index
+        database.execSQL("ALTER TABLE event_group_temp RENAME TO event_group")
+        database.execSQL("CREATE INDEX index_event_group_wallet_id ON event_group(wallet_id)")
+        database.execSQL("CREATE UNIQUE INDEX index_event_group_provider_identifier_type_scope ON event_group(provider_identifier, type, scope)")
+    }
+}
+
 @Database(
     entities = [WalletEntity::class, EventGroupEntity::class, GreenCardEntity::class, CredentialEntity::class, OriginEntity::class],
-    version = 4
+    version = 5
 )
 @TypeConverters(HolderDatabaseConverter::class)
 abstract class HolderDatabase : RoomDatabase() {
@@ -57,7 +80,7 @@ abstract class HolderDatabase : RoomDatabase() {
                 SupportFactory(SQLiteDatabase.getBytes(secretKeyUseCase.json().toCharArray()))
             return Room
                 .databaseBuilder(context, HolderDatabase::class.java, "holder-database")
-                .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4)
+                .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5)
                 .apply {
                     if (isProd) {
                         openHelperFactory(supportFactory)
