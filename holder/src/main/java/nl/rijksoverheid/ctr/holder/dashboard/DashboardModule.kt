4d449ec5dfc8f8387caf055dcfc92e415fefbc20
Giorgos Papadopoulos <giorgos@giorgos.nl>
2022-04-29 15:28:10 +0200

    Revert "4211"
    This reverts commit d6d353a130c42a7263761728540d957282023e0d.

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/dashboard/DashboardModule.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/dashboard/DashboardModule.kt
@@ -9,11 +9,8 @@ package nl.rijksoverheid.ctr.holder.dashboard
 
 import nl.rijksoverheid.ctr.holder.dashboard.datamappers.DashboardTabsItemDataMapper
 import nl.rijksoverheid.ctr.holder.dashboard.datamappers.DashboardTabsItemDataMapperImpl
-import nl.rijksoverheid.ctr.holder.dashboard.util.DashboardPageAccessibilityUtil
-import nl.rijksoverheid.ctr.holder.dashboard.util.DashboardPageAccessibilityUtilImpl
 import org.koin.dsl.module
 
 val dashboardModule = module {
     factory<DashboardTabsItemDataMapper> { DashboardTabsItemDataMapperImpl(get()) }
-    factory<DashboardPageAccessibilityUtil> { DashboardPageAccessibilityUtilImpl() }
 }
--- holder/src/main/java/nl/rijksoverheid/ctr/holder/dashboard/DashboardPageFragment.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/dashboard/DashboardPageFragment.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ * Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
 package nl.rijksoverheid.ctr.holder.dashboard
 
 import android.os.Bundle
@@ -14,7 +21,6 @@ import nl.rijksoverheid.ctr.holder.databinding.FragmentDashboardPageBinding
 import nl.rijksoverheid.ctr.persistence.database.entities.GreenCardType
 import nl.rijksoverheid.ctr.holder.dashboard.models.DashboardItem
 import nl.rijksoverheid.ctr.holder.dashboard.util.CardItemUtil
-import nl.rijksoverheid.ctr.holder.dashboard.util.DashboardPageAccessibilityUtil
 import nl.rijksoverheid.ctr.holder.qrcodes.models.QrCodeFragmentData
 import nl.rijksoverheid.ctr.holder.dashboard.util.DashboardPageInfoItemHandlerUtil
 import nl.rijksoverheid.ctr.shared.ext.findNavControllerSafety
@@ -50,7 +56,6 @@ class DashboardPageFragment : Fragment(R.layout.fragment_dashboard_page) {
 
     }
 
-    private val dashboardPageAccessibilityUtil: DashboardPageAccessibilityUtil by inject()
     private val dashboardPageInfoItemHandlerUtil: DashboardPageInfoItemHandlerUtil by inject()
     private val cardItemUtil: CardItemUtil by inject()
     val dashboardViewModel: DashboardViewModel by sharedViewModelWithOwner(owner = {
@@ -109,9 +114,6 @@ class DashboardPageFragment : Fragment(R.layout.fragment_dashboard_page) {
             }
         }
 
-        if (section.groups.isNotEmpty()) {
-            dashboardPageAccessibilityUtil.announceNewInfoItems(section.groups, adapterItems)
-        }
         section.update(adapterItems)
     }
 
