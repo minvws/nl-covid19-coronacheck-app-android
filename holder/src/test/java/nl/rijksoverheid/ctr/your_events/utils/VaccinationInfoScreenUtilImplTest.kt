53b3e4d8ef56cd2eaa424af4cf0bfd096c650a02
Bart Nijland <bartnijland91@gmail.com>
2022-05-02 11:06:59 +0200

    Added test

--- holder/src/test/java/nl/rijksoverheid/ctr/your_events/utils/VaccinationInfoScreenUtilImplTest.kt
+++ holder/src/test/java/nl/rijksoverheid/ctr/your_events/utils/VaccinationInfoScreenUtilImplTest.kt
@@ -10,12 +10,10 @@ import nl.rijksoverheid.ctr.persistence.HolderCachedAppConfigUseCase
 import nl.rijksoverheid.ctr.holder.qrcodes.utils.LastVaccinationDoseUtil
 import nl.rijksoverheid.ctr.holder.qrcodes.utils.LastVaccinationDoseUtilImpl
 import nl.rijksoverheid.ctr.holder.get_events.models.RemoteEventVaccination
-import nl.rijksoverheid.ctr.holder.paper_proof.usecases.GetDccFromEuropeanCredentialUseCase
 import nl.rijksoverheid.ctr.holder.paper_proof.utils.PaperProofUtil
 import nl.rijksoverheid.ctr.holder.utils.CountryUtil
 import nl.rijksoverheid.ctr.holder.your_events.utils.VaccinationInfoScreenUtilImpl
 import nl.rijksoverheid.ctr.shared.MobileCoreWrapper
-import org.json.JSONObject
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -206,6 +204,93 @@ class VaccinationInfoScreenUtilImplTest : AutoCloseKoinTest() {
         assertEquals("Details", infoScreen.title)
         assertEquals(
             "Deze gegevens staan in je papieren bewijs:<br/><br/>Naam: <b>Surname</b><br/>Geboortedatum: <b>01-08-1982</b><br/><br/>Ziekteverwekker: <b>COVID-19</b><br/>Vaccin: <b>Pfizer (Comirnaty)</b><br/>Type vaccin: <b>type</b><br/>Producent: <b>Biontech Manufacturing GmbH</b><br/>Dosis: <b>1 / 2</b><br/>Vaccinatiedatum: <b>1 augustus 2021</b><br/>Gevaccineerd in: <b>Nederland</b><br/>Afgever certificaat: <b>Bart</b><br/>Uniek vaccinatienummer: <b>unique</b><br/><br /><b>Kloppen er gegevens niet?</b><br />Neem contact op met de zorgverlener waar je bent getest of gevaccineerd. Vraag of zij je kunnen helpen je gegevens aan te passen.",
+            infoScreen.description
+        )
+    }
+
+    @Test
+    fun `get correct vaccination screen info for non foreign paper proof`() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val resources = context.resources
+
+        val mobileCoreWrapper: MobileCoreWrapper = mockk()
+        every { mobileCoreWrapper.isForeignDcc(any()) } answers { false }
+
+        val paperProofUtil: PaperProofUtil = mockk()
+        every { paperProofUtil.getIssuer(any()) } answers { "Bart" }
+
+        val lastVaccinationDoseUtil = LastVaccinationDoseUtilImpl(resources)
+        val cachedAppConfigUseCase: HolderCachedAppConfigUseCase = object : HolderCachedAppConfigUseCase {
+            override fun getCachedAppConfigOrNull(): HolderConfig? {
+                return null
+            }
+
+            override fun getCachedAppConfig() = HolderConfig.default().copy(
+                hpkCodes = listOf(
+                    AppConfig.HpkCode(
+                        code = "2924528",
+                        name = "Pfizer (Comirnaty)",
+                        vp = "1119349007",
+                        mp = "EU/1/20/1528",
+                        ma = "ORG-100030215"
+                    )
+                ),
+                euBrands = listOf(
+                    AppConfig.Code(
+                        code = "EU/1/20/1528",
+                        name = "Pfizer (Comirnaty)",
+                    )
+                ),
+                euManufacturers = listOf(
+                    AppConfig.Code(
+                        code = "ORG-100030215",
+                        name = "Biontech Manufacturing GmbH"
+                    )
+                )
+            )
+        }
+
+        val vaccinationClock =
+            Clock.fixed(Instant.parse("2021-08-01T00:00:00.00Z"), ZoneId.of("UTC"))
+
+        val event = RemoteEventVaccination(
+            type = "vaccination",
+            unique = "unique",
+            vaccination = RemoteEventVaccination.Vaccination(
+                date = LocalDate.now(vaccinationClock),
+                hpkCode = "2924528",
+                type = "type",
+                brand = "brand",
+                completedByMedicalStatement = true,
+                completedByPersonalStatement = false,
+                completionReason = "completionReason",
+                doseNumber = "1",
+                totalDoses = "2",
+                country = "NLD",
+                manufacturer = "manufacturer",
+            )
+        )
+
+        val vaccinationInfoScreenUtil = VaccinationInfoScreenUtilImpl(
+            lastVaccinationDoseUtil,
+            resources,
+            countryUtil,
+            paperProofUtil,
+            mobileCoreWrapper,
+            cachedAppConfigUseCase
+        )
+
+        val infoScreen = vaccinationInfoScreenUtil.getForVaccination(
+            event = event,
+            fullName = "Surname",
+            birthDate = "01-08-1982",
+            providerIdentifier = "GGD",
+            europeanCredential = "".toByteArray()
+        )
+
+        assertEquals("Details", infoScreen.title)
+        assertEquals(
+            "Deze gegevens staan in je papieren bewijs:<br/><br/>Naam: <b>Surname</b><br/>Geboortedatum: <b>01-08-1982</b><br/><br/>Ziekteverwekker: <b>COVID-19</b><br/>Vaccin: <b>Pfizer (Comirnaty)</b><br/>Type vaccin: <b>type</b><br/>Producent: <b>Biontech Manufacturing GmbH</b><br/>Dosis: <b>1 / 2</b><br/>Vaccinatiedatum: <b>1 augustus 2021</b><br/>Gevaccineerd in: <b>Nederland</b><br/>Afgever certificaat: <b>Bart</b><br/>Uniek vaccinatienummer: <b>unique</b><br/><br/><b>Kloppen er gegevens niet?</b><br/>Neem contact op met de zorgverlener waar je bent gevaccineerd of getest. Dat kan de GGD, je huisarts of arts van een andere zorginstelling zijn. Zij kunnen je helpen je gegevens aan te passen.",
             infoScreen.description
         )
     }
