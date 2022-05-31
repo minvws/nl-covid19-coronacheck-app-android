84963c1f4a7692ca160c60d73b1961807b7e9f0f
Bart Nijland <bartnijland91@gmail.com>
2022-05-31 15:58:18 +0200

    Added more screenshot tests for different green card scenarios

--- holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/Fakes.kt
+++ holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/Fakes.kt
@@ -13,10 +13,37 @@ import nl.rijksoverheid.ctr.appconfig.models.AppStatus
 import nl.rijksoverheid.ctr.holder.dashboard.DashboardViewModel
 import nl.rijksoverheid.ctr.holder.dashboard.models.DashboardSync
 import nl.rijksoverheid.ctr.holder.dashboard.models.DashboardTabItem
-import nl.rijksoverheid.ctr.persistence.database.entities.GreenCardType
-import nl.rijksoverheid.ctr.persistence.database.entities.OriginEntity
+import nl.rijksoverheid.ctr.persistence.database.entities.*
+import nl.rijksoverheid.ctr.persistence.database.models.GreenCard
 import nl.rijksoverheid.ctr.shared.MobileCoreWrapper
 import nl.rijksoverheid.ctr.shared.models.DisclosurePolicy
+import java.time.OffsetDateTime
+
+fun fakeGreenCardEntity(
+    id: Int = 0,
+    walletId: Int = 0,
+    type: GreenCardType = GreenCardType.Domestic
+) = GreenCardEntity(id, walletId, type)
+
+fun fakeOriginEntity(
+    id: Int = 0,
+    greenCardId: Long = 1L,
+    type: OriginType = OriginType.Vaccination,
+    eventTime: OffsetDateTime = OffsetDateTime.now(),
+    expirationTime: OffsetDateTime = OffsetDateTime.now(),
+    validFrom: OffsetDateTime = OffsetDateTime.now(),
+    doseNumber: Int? = null
+) = OriginEntity(id, greenCardId, type, eventTime, expirationTime, validFrom, doseNumber)
+
+fun fakeCredentialEntity(
+    id: Long = 0,
+    greenCardId: Long = 1L,
+    data: ByteArray = "".toByteArray(),
+    credentialVersion: Int = 1,
+    validFrom: OffsetDateTime = OffsetDateTime.now(),
+    expirationTime: OffsetDateTime = OffsetDateTime.now(),
+    category: String? = null
+) = CredentialEntity(id, greenCardId, data, credentialVersion, validFrom, expirationTime, category)
 
 fun fakeAppConfigViewModel(appStatus: AppStatus = AppStatus.NoActionRequired) =
     object : AppConfigViewModel() {
