cd0191b78a6f29dc0de8ce2b1b0bb6799ae0bae3
Giorgos Papadopoulos <giorgos@giorgos.nl>
2022-07-12 10:55:30 +0200

    fix tests

--- holder/src/test/java/nl/rijksoverheid/ctr/holder/no_digid/NoDigidScreenDataUtilImplTest.kt
+++ holder/src/test/java/nl/rijksoverheid/ctr/holder/no_digid/NoDigidScreenDataUtilImplTest.kt
@@ -2,9 +2,11 @@ package nl.rijksoverheid.ctr.holder.no_digid
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
+import io.mockk.every
+import io.mockk.mockk
 import nl.rijksoverheid.ctr.holder.R
 import nl.rijksoverheid.ctr.holder.get_events.models.RemoteOriginType
-import nl.rijksoverheid.ctr.holder.models.HolderFlow
+import nl.rijksoverheid.ctr.holder.usecases.HolderFeatureFlagUseCase
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,7 +17,9 @@ import org.robolectric.RobolectricTestRunner
 class NoDigidScreenDataUtilImplTest : AutoCloseKoinTest() {
     private val applicationContext: Context by lazy { ApplicationProvider.getApplicationContext() }
 
-    private val noDigidScreenDataUtil = NoDigidScreenDataUtilImpl(applicationContext)
+    private val noDigidScreenDataUtil = NoDigidScreenDataUtilImpl(applicationContext, mockk<HolderFeatureFlagUseCase>().apply {
+        every { getPapEnabled() } returns true
+    })
 
     @Test
     fun `requestDigidButton has correct data`() {
@@ -40,7 +44,7 @@ class NoDigidScreenDataUtilImplTest : AutoCloseKoinTest() {
         assertEquals(R.string.holder_checkForBSN_buttonTitle_doesHaveBSN, firstButtonData.title)
         assertEquals(applicationContext.getString(R.string.holder_checkForBSN_buttonSubTitle_doesHaveBSN), firstButtonData.subtitle)
         assertEquals(R.string.holder_checkForBSN_buttonTitle_doesNotHaveBSN, secondButtonData.title)
-        assertEquals(applicationContext.getString(R.string.holder_checkForBSN_buttonSubTitle_doesNotHaveBSN), secondButtonData.subtitle)
+        assertEquals(applicationContext.getString(R.string.holder_checkForBSN_buttonSubTitle_doesNotHaveBSN_testFlow), secondButtonData.subtitle)
         assertEquals(applicationContext.getString(R.string.holder_contactCoronaCheckHelpdesk_title), firstButtonData.infoFragmentData.title)
         assertEquals(R.string.holder_contactCoronaCheckHelpdesk_message, firstButtonData.infoFragmentData.descriptionData.htmlText)
     }
