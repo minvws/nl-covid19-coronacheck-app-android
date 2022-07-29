7247986bf158e9d5212724264fd917e59e50f589
Giorgos Papadopoulos <giorgos@giorgos.nl>
2022-07-29 16:17:40 +0200

    apply spotless fixes before commit

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/get_events/LoginViewModel.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/get_events/LoginViewModel.kt
@@ -125,8 +125,8 @@ class LoginViewModel(
 
     private fun isUserCancelled(authError: AuthorizationException) =
         (authError.type == AuthorizationException.TYPE_GENERAL_ERROR && authError.code == USER_CANCELLED_FLOW_CODE) ||
-                authError.error == SAML_AUTHN_FAILED_ERROR
-                || authError.error == CANCELLED
+                authError.error == SAML_AUTHN_FAILED_ERROR ||
+                authError.error == CANCELLED
 
     private suspend fun postAuthResponseResult(
         loginType: LoginType,
