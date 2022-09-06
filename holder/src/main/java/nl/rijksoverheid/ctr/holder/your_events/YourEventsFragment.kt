1161254a87ed9f848735033c0df46cff1fb288c3
Giorgos Papadopoulos <giorgos@giorgos.nl>
2022-09-06 18:16:05 +0300

    fallback to the default text button title

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/your_events/YourEventsFragment.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/your_events/YourEventsFragment.kt
@@ -615,9 +615,16 @@ class YourEventsFragment : BaseFragment(R.layout.fragment_your_events) {
         binding.bottom.setButtonClick {
             retrieveGreenCards()
         }
-        if (getFlow() == HolderFlow.VaccinationAssessment) {
-            binding.bottom.setButtonText(getString(R.string.holder_event_vaccination_assessment_action_title))
-        }
+        binding.bottom.setButtonText(
+            getString(
+                when {
+                    getFlow() == HolderFlow.VaccinationAssessment -> {
+                        R.string.holder_event_vaccination_assessment_action_title
+                    }
+                    else -> R.string.your_negative_test_results_row_button
+                }
+            )
+        )
     }
 
     private fun presentHeader(binding: FragmentYourEventsBinding) {
