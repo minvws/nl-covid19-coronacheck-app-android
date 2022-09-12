8317d062272324c0c8f6a40cb94db5bcaa7b4133
Giorgos Papadopoulos <giorgos@giorgos.nl>
2022-09-12 20:35:23 +0300

    use private root ca for cms signing

--- api/src/main/java/nl/rijksoverheid/ctr/api/interceptors/SignedResponseInterceptor.kt
+++ api/src/main/java/nl/rijksoverheid/ctr/api/interceptors/SignedResponseInterceptor.kt
@@ -45,6 +45,7 @@ class SignedResponseInterceptor(
 ) : Interceptor {
     private val defaultValidator = SignatureValidator.Builder()
         .addTrustedCertificate(EV_ROOT_CA)
+        .addTrustedCertificate(PRIVATE_ROOT_CA)
         .cnMatching(signatureCertificateCnMatch)
         .build()
 
