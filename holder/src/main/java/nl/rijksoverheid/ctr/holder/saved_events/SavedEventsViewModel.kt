1cc525e9db68155733550e3c6dd012bafd6bd752
Giorgos Papadopoulos <giorgos@giorgos.nl>
2022-10-17 15:06:12 +0200

    don't show fuzzy matching banner if user deleted the events he chose to store

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/saved_events/SavedEventsViewModel.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/saved_events/SavedEventsViewModel.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.launch
 import nl.rijksoverheid.ctr.holder.saved_events.usecases.GetSavedEventsUseCase
 import nl.rijksoverheid.ctr.persistence.database.HolderDatabase
 import nl.rijksoverheid.ctr.persistence.database.entities.EventGroupEntity
+import nl.rijksoverheid.ctr.persistence.database.entities.RemovedEventReason
 import nl.rijksoverheid.ctr.shared.livedata.Event
 
 class SavedEventsViewModel(
@@ -36,6 +37,9 @@ class SavedEventsViewModel(
     fun removeSavedEvents(eventGroupEntity: EventGroupEntity) {
         viewModelScope.launch {
             holderDatabase.eventGroupDao().delete(eventGroupEntity)
+            // the user deleted consciously a stored event and is aware of the currently stored events
+            // so no point to communicate anymore which conflicted events were deleted
+            holderDatabase.removedEventDao().deleteAll(RemovedEventReason.FuzzyMatched)
             (removedSavedEventsLiveData as MutableLiveData).postValue(Unit)
         }
     }
