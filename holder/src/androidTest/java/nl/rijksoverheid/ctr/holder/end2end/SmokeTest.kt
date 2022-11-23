ed7a8b4b0f24e1064d7c418807366982e9f8b518
Joris De Haes <joris@egeniq.com>
2022-11-23 09:59:55 +0100

    Update SmokeTest.kt

--- holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/end2end/SmokeTest.kt
+++ holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/end2end/SmokeTest.kt
@@ -1,13 +1,13 @@
 package nl.rijksoverheid.ctr.holder.end2end
 
-import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import nl.rijksoverheid.ctr.holder.end2end.utils.Elements.checkForText
 import org.junit.Test
 
 class SmokeTest : BaseTest() {
 
     @Test
     fun start_app_and_see_overview() {
-        assertDisplayed("My certificates")
-        assertDisplayed("Menu")
+        checkForText("My certificates")
+        checkForText("Menu")
     }
 }
