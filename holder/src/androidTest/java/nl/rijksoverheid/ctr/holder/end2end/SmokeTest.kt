d49d5a3d9b1a0ca029438398f0e46cb5235b400d
Joris De Haes <joris@egeniq.com>
2022-11-29 16:53:45 +0100

    Skip onboarding and similar screens

--- holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/end2end/SmokeTest.kt
+++ holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/end2end/SmokeTest.kt
@@ -1,6 +1,6 @@
 package nl.rijksoverheid.ctr.holder.end2end
 
-import nl.rijksoverheid.ctr.holder.end2end.utils.Elements.checkForText
+import nl.rijksoverheid.ctr.holder.end2end.utils.Assertions.assertOverview
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
@@ -9,8 +9,7 @@ class SmokeTest : BaseTest() {
 
     @Test
     fun start_app_and_see_overview() {
-        checkForText("My certificates")
-        checkForText("Menu")
+        assertOverview()
     }
 
     @Test
