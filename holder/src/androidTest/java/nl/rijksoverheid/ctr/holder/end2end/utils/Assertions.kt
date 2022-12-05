77a8346b53b6c7999b26c463846881d91f123d23
Joris De Haes <joris@egeniq.com>
2022-12-05 16:11:59 +0100

    Remove unused clickOn

--- holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/end2end/utils/Assertions.kt
+++ holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/end2end/utils/Assertions.kt
@@ -3,7 +3,6 @@ package nl.rijksoverheid.ctr.holder.end2end.utils
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertContains
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import com.adevinta.android.barista.interaction.BaristaClickInteractions.clickBack
-import com.adevinta.android.barista.interaction.BaristaClickInteractions.clickOn
 import java.time.temporal.ChronoUnit
 import nl.rijksoverheid.ctr.holder.end2end.BaseTest.Companion.today
 import nl.rijksoverheid.ctr.holder.end2end.model.Event
@@ -22,7 +21,6 @@ object Assertions {
     fun assertOverview() {
         waitForText("Mijn bewijzen")
         assertDisplayed("Menu")
-        clickOn("Internationaal")
     }
 
     fun assertRetrievalDetails(person: Person, event: Event, position: Int = 0) {
