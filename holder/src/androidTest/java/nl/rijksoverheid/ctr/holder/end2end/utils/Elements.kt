64e2bbe1a79a1b62f6262874f2894cde1bd59a31
Joris De Haes <joris@egeniq.com>
2022-12-19 10:55:10 +0100

    Additional elements and logging

--- holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/end2end/utils/Elements.kt
+++ holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/end2end/utils/Elements.kt
@@ -13,9 +13,12 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiObject
 import androidx.test.uiautomator.UiObject2
+import androidx.test.uiautomator.UiScrollable
 import androidx.test.uiautomator.UiSelector
 import androidx.test.uiautomator.Until
 import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertContains
+import com.adevinta.android.barista.interaction.BaristaListInteractions.scrollListToPosition
+import java.lang.Thread.sleep
 import junit.framework.TestCase.assertNotNull
 import nl.rijksoverheid.ctr.holder.R
 import nl.rijksoverheid.ctr.holder.end2end.BaseTest.Companion.device
@@ -25,24 +28,30 @@ import org.hamcrest.CoreMatchers.containsStringIgnoringCase
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.TypeSafeMatcher
+import timber.log.Timber
 
 object Elements {
 
     fun waitForText(text: String, timeout: Long = 5): UiObject2? {
-        val element = device.wait(Until.findObject(By.textContains(text)), timeout * 1000)
+        Timber.tag("end2end").d("Waiting for text '$text'")
+        val element = device.wait(Until.findObject(By.textStartsWith(text)), timeout * 1000)
         assertNotNull("'$text' could not be found", element)
         return element
     }
 
-    fun checkForText(text: String, timeout: Long = 5): Boolean {
-        return device.wait(Until.hasObject(By.textStartsWith(text)), timeout * 1000)!!
+    fun checkForText(text: String, timeout: Long = 1): Boolean {
+        val textFound = device.wait(Until.hasObject(By.textContains(text)), timeout * 1000)!!
+        Timber.tag("end2end").d("Checking if text '$text' was found: $textFound")
+        return textFound
     }
 
     fun tapButton(label: String, position: Int = 0) {
+        Timber.tag("end2end").d("Tapping button with label '$label'${if (position > 0) " on position $position" else ""}")
         onView(allOf(withIndex(withText(containsStringIgnoringCase(label)), position))).perform(click())
     }
 
     fun labelValuePairExist(label: String, value: String) {
+        Timber.tag("end2end").d("Asserting label '$label' with value '$value'")
         assertContains("$label\n$value")
     }
 
@@ -68,7 +77,7 @@ object Elements {
     }
 
     fun card(eventType: Event.Type): Matcher<View> {
-        val text = eventType.domesticName
+        val text = eventType.internationalName
         return allOf(
             withId(R.id.proof_1),
             hasDescendant(
@@ -81,14 +90,60 @@ object Elements {
     }
 
     fun Matcher<View>.containsText(text: String) {
+        Timber.tag("end2end").d("Asserting text '$text' on a view ")
         onView(allOf(isDescendantOfA(this), withText(containsStringIgnoringCase(text)))).check(ViewAssertions.matches(isDisplayed()))
     }
 
-    fun Matcher<View>.tapButton(text: String) {
-        onView(allOf(isDescendantOfA(this), withText(containsStringIgnoringCase(text)))).perform(click())
+    fun Matcher<View>.tapButton(label: String) {
+        Timber.tag("end2end").d("Tapping button with label '$label' on a view")
+        onView(allOf(isDescendantOfA(this), withText(containsStringIgnoringCase(label)))).perform(click())
     }
 
-    fun findElement(className: Class<*>, label: String): UiObject? {
-        return device.findObject(UiSelector().className(className).text(label))
+    fun checkTextExists(text: String): Boolean {
+        Timber.tag("end2end").d("Check if text '$text' exists")
+        val element = device.findObject(UiSelector().textContains(text)).waitForExists(3)
+        Timber.tag("end2end").d("  Result: $element")
+        return element
+    }
+
+    fun enterBsn(bsn: String) {
+        Timber.tag("end2end").d("Enter bsn '$bsn'")
+        val browserWindow = UiScrollable(UiSelector().scrollable(true))
+        val element = browserWindow.getChild(UiSelector().className(android.widget.EditText::class.java))
+        element!!.click()
+        element.text = bsn
+        device.pressEnter()
+        rest()
+    }
+
+    fun enterTextInField(index: Int, text: String): UiObject {
+        Timber.tag("end2end").d("Find EditText element with index '$index' and entering text")
+        val element = device.findObject(UiSelector().className(android.widget.EditText::class.java).instance(index))
+        assertNotNull("EditText element with index '$index' could not be found", element)
+        element!!.click()
+        element.text = text
+        return element
+    }
+
+    fun tapButtonElement(label: String) {
+        Timber.tag("end2end").d("Find Button element with label '$label' and clicking")
+        val element = device.findObject(UiSelector().className(android.widget.Button::class.java).textContains(label))
+        assertNotNull("Button element with label '$label' could not be found", element)
+        element!!.click()
+    }
+
+    @Suppress("unused")
+    fun Any.rest(timeout: Long = 1) {
+        Timber.tag("end2end").d("  Rest Here Weary Traveler, for just ${if (timeout > 1) "$timeout seconds" else "a second"}")
+        sleep(timeout * 1000)
+    }
+
+    fun scrollToTextInOverview(text: String) {
+        for (i in 2..12 step 2) {
+            Timber.tag("end2end").d("Scrolling to position $i on overview to search for '$text'")
+            scrollListToPosition(R.id.recyclerView, i)
+            val found = device.findObject(By.textContains(text)) != null
+            if (found) break
+        }
     }
 }
