80eb683c55762a8fb99cc2df046bc4a523767b5b
Joris De Haes <joris@egeniq.com>
2022-12-19 12:00:59 +0100

    Remove test for password

--- holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/end2end/SmokeTest.kt
+++ holder/src/androidTest/java/nl/rijksoverheid/ctr/holder/end2end/SmokeTest.kt
@@ -1,8 +1,6 @@
 package nl.rijksoverheid.ctr.holder.end2end
 
 import nl.rijksoverheid.ctr.holder.end2end.utils.Assertions.assertOverview
-import org.hamcrest.CoreMatchers.notNullValue
-import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
 class SmokeTest : BaseTest() {
@@ -10,10 +8,5 @@ class SmokeTest : BaseTest() {
     @Test
     fun start_app_and_see_overview() {
         assertOverview()
-    }
-
-    @Test
-    fun backend_password_is_available() {
-        assertThat(authPassword, notNullValue())
     }
 }
