8ccc48220cf56542dbe13bf8a82f92e896e39a45
Giorgos Papadopoulos <giorgos@giorgos.nl>
2023-02-13 13:50:37 +0100

    replace navigation animations to respect system settings

---
+++ design/src/main/java/nl/rijksoverheid/ctr/design/widgets/AnimatorFrameLayout.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ * Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+package nl.rijksoverheid.ctr.design.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import nl.rijksoverheid.ctr.shared.ext.getDisplaySize
+
+class AnimatorFrameLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    // it's used by the object animator of the fragment transition animations
+    @Suppress("unused")
+    fun setPercentageX(percentage: Float) {
+        val width = context.getDisplaySize().width
+        x = if (width > 0) {
+            percentage * width
+        } else {
+            0f
+        }
+    }
+}
