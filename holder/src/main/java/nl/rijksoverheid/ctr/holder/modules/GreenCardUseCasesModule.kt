7ad82eff51fe273c859f834d043b4ac43c1c985f
Giorgos Papadopoulos <giorgos@giorgos.nl>
2023-06-15 16:21:46 +0200

    can add events based on feature flag

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/modules/GreenCardUseCasesModule.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/modules/GreenCardUseCasesModule.kt
@@ -38,7 +38,7 @@ val greenCardUseCasesModule = module {
         CreateEuGreenCardUseCaseImpl(get(), get())
     }
     factory<GetDashboardItemsUseCase> {
-        GetDashboardItemsUseCaseImpl(get(), get(), get(), get(), get(), get(), get(), get(), get())
+        GetDashboardItemsUseCaseImpl(get(), get(), get(), get(), get(), get(), get(), get(), get(), get())
     }
     factory<SortGreenCardItemsUseCase> {
         SortGreenCardItemsUseCaseImpl(get(), get())
