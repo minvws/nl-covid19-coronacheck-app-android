f6d8110ee26b3f4cb1f2c36a4c6737092fe1fe94
Giorgos Papadopoulos <giorgos@giorgos.nl>
2023-06-17 01:58:41 +0200

    share exported pdf

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/pdf/PdfExportedFragment.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/pdf/PdfExportedFragment.kt
@@ -8,14 +8,18 @@
 
 package nl.rijksoverheid.ctr.holder.pdf
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
+import java.io.File
 import nl.rijksoverheid.ctr.holder.R
 import nl.rijksoverheid.ctr.holder.databinding.FragmentPdfExportedBinding
 import nl.rijksoverheid.ctr.shared.ext.navigateSafety
 
-class PdfExportedFragment: Fragment(R.layout.fragment_pdf_exported) {
+class PdfExportedFragment : Fragment(R.layout.fragment_pdf_exported) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -26,7 +30,22 @@ class PdfExportedFragment: Fragment(R.layout.fragment_pdf_exported) {
         }
 
         binding.savePdfButton.setOnClickListener {
-
+            val pdfFile = File(requireContext().filesDir, PdfWebViewFragment.pdfFileName)
+            startActivity(
+                Intent.createChooser(Intent().apply {
+                    action = Intent.ACTION_SEND
+                    type = "application/pdf"
+                    putExtra(
+                        Intent.EXTRA_STREAM,
+                        FileProvider.getUriForFile(
+                            requireContext(),
+                            requireContext().applicationContext.packageName + ".provider",
+                            pdfFile
+                        )
+                    )
+                    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                }, getString(R.string.holder_pdfExport_success_card_action_save))
+            )
         }
     }
 }
