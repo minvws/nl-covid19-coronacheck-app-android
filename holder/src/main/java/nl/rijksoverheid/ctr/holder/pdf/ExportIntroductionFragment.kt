00f32293e72e9e14221079f518411a86a310c60a
Giorgos Papadopoulos <giorgos@giorgos.nl>
2023-06-17 22:34:54 +0200

    5647

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/pdf/ExportIntroductionFragment.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/pdf/ExportIntroductionFragment.kt
@@ -10,18 +10,28 @@ package nl.rijksoverheid.ctr.holder.pdf
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import nl.rijksoverheid.ctr.holder.R
 import nl.rijksoverheid.ctr.holder.databinding.FragmentExportIntroductionBinding
 import nl.rijksoverheid.ctr.shared.ext.navigateSafety
+import nl.rijksoverheid.ctr.shared.utils.AndroidUtil
+import org.koin.android.ext.android.inject
 
 class ExportIntroductionFragment : Fragment(R.layout.fragment_export_introduction) {
+
+    private val androidUtil: AndroidUtil by inject()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val binding = FragmentExportIntroductionBinding.bind(view)
         binding.bottom.setButtonClick {
             navigateSafety(ExportIntroductionFragmentDirections.actionPdfWebview())
+        }
+
+        if (androidUtil.isSmallScreen()) {
+            binding.image.isVisible = false
         }
     }
 }
