66dbba01fabc08b63bd28b13634591eb61e954a4
Giorgos Papadopoulos <giorgos@giorgos.nl>
2023-06-17 23:45:57 +0200

    improve pdf preview

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/pdf/PdfPreviewFragment.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/pdf/PdfPreviewFragment.kt
@@ -8,6 +8,7 @@
 package nl.rijksoverheid.ctr.holder.pdf
 
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.pdf.PdfRenderer
 import android.os.Bundle
 import android.os.ParcelFileDescriptor
@@ -28,16 +29,35 @@ class PdfPreviewFragment : Fragment(R.layout.fragment_pdf_preview) {
             val parcelFileDescriptor =
                 ParcelFileDescriptor.open(pdfFile, ParcelFileDescriptor.MODE_READ_ONLY)
             val pdfRenderer = PdfRenderer(parcelFileDescriptor)
-            val currentPage = pdfRenderer.openPage(0)
-            val bitmap = Bitmap.createBitmap(
-                currentPage.width,
-                currentPage.height,
-                Bitmap.Config.ARGB_8888
-            )
-            currentPage.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
-            binding.pdfImageView.setImageBitmap(bitmap)
+            val bitmaps = mutableListOf<Bitmap>()
+            for (i in 0 until pdfRenderer.pageCount) {
+                val currentPage = pdfRenderer.openPage(i)
+                val bitmap = Bitmap.createBitmap(
+                    currentPage.width,
+                    currentPage.height,
+                    Bitmap.Config.ARGB_8888
+                )
+                currentPage.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+                bitmaps.add(bitmap)
+                currentPage.close()
+            }
+            pdfRenderer.close()
+            binding.pdfImageView.setImageBitmap(pdfBitmap(bitmaps))
         } catch (exception: Exception) {
             exception.printStackTrace()
         }
+    }
+
+    fun pdfBitmap(bitmaps: List<Bitmap>): Bitmap {
+        val width = bitmaps.first().width
+        val pageHeight = bitmaps.first().height
+        val height = bitmaps.first().height * bitmaps.size
+        val comboBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val comboImage = Canvas(comboBitmap)
+        bitmaps.forEachIndexed { index, bitmap ->
+            comboImage.drawBitmap(bitmap, 0f, (index * pageHeight).toFloat(), null)
+        }
+
+        return comboBitmap
     }
 }
