931520b5496560eed6ef47b216dcf60a92f6014a
Giorgos Papadopoulos <giorgos@giorgos.nl>
2023-12-01 14:51:40 +0100

    don't use config

--- holder/src/main/java/nl/rijksoverheid/ctr/holder/HolderMainActivity.kt
+++ holder/src/main/java/nl/rijksoverheid/ctr/holder/HolderMainActivity.kt
@@ -11,17 +11,7 @@ package nl.rijksoverheid.ctr.holder
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import nl.rijksoverheid.ctr.design.utils.DialogButtonData
-import nl.rijksoverheid.ctr.design.utils.DialogFragmentData
-import nl.rijksoverheid.ctr.design.utils.DialogUtil
-import nl.rijksoverheid.ctr.design.utils.SharedDialogFragment
 import nl.rijksoverheid.ctr.holder.databinding.ActivityMainBinding
-import nl.rijksoverheid.ctr.holder.ui.device_rooted.DeviceRootedViewModel
-import nl.rijksoverheid.ctr.holder.ui.device_secure.DeviceSecureViewModel
-import nl.rijksoverheid.ctr.holder.ui.priority_notification.PriorityNotificationViewModel
-import nl.rijksoverheid.ctr.shared.livedata.EventObserver
-import org.koin.android.ext.android.inject
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /*
  *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
@@ -31,11 +21,6 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
  *
  */
 class HolderMainActivity : AppCompatActivity() {
-
-    private val deviceRootedViewModel: DeviceRootedViewModel by viewModel()
-    private val deviceSecureViewModel: DeviceSecureViewModel by viewModel()
-    private val priorityNotificationViewModel: PriorityNotificationViewModel by viewModel()
-    private val dialogUtil: DialogUtil by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme_DayNight)
@@ -49,44 +34,5 @@ class HolderMainActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_SECURE
             )
         }
-
-        deviceRootedViewModel.deviceRootedLiveData.observe(this, EventObserver {
-            if (it) {
-                dialogUtil.presentDialog(
-                    context = this,
-                    title = R.string.dialog_rooted_device_title,
-                    message = getString(R.string.dialog_rooted_device_message),
-                    positiveButtonText = R.string.dialog_rooted_device_positive_button,
-                    positiveButtonCallback = { },
-                    onDismissCallback = { deviceRootedViewModel.setHasDismissedRootedDeviceDialog() }
-                )
-            }
-        })
-
-        deviceSecureViewModel.deviceSecureLiveData.observe(this, EventObserver {
-            if (!it) {
-                dialogUtil.presentDialog(
-                    context = this,
-                    title = R.string.dialog_device_secure_warning_title,
-                    message = getString(R.string.dialog_device_secure_warning_description),
-                    positiveButtonText = R.string.dialog_device_secure_positive_button,
-                    positiveButtonCallback = { },
-                    onDismissCallback = {
-                        deviceSecureViewModel.setHasDismissedUnsecureDeviceDialog(
-                            true
-                        )
-                    }
-                )
-            }
-        })
-
-        priorityNotificationViewModel.showPriorityNotificationLiveData.observe(this, EventObserver {
-            SharedDialogFragment.show(
-                supportFragmentManager, DialogFragmentData(
-                    text = it,
-                    positiveButtonData = DialogButtonData.Dismiss(R.string.ok)
-                )
-            )
-        })
     }
 }
--- introduction/src/main/java/nl/rijksoverheid/ctr/introduction/setup/SetupFragment.kt
+++ introduction/src/main/java/nl/rijksoverheid/ctr/introduction/setup/SetupFragment.kt
@@ -3,14 +3,8 @@ package nl.rijksoverheid.ctr.introduction.setup
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import nl.rijksoverheid.ctr.appconfig.AppConfigViewModel
-import nl.rijksoverheid.ctr.appconfig.models.AppStatus
 import nl.rijksoverheid.ctr.introduction.R
-import nl.rijksoverheid.ctr.shared.MobileCoreWrapper
-import nl.rijksoverheid.ctr.shared.ext.findNavControllerSafety
 import nl.rijksoverheid.ctr.shared.utils.Accessibility
-import org.koin.android.ext.android.inject
-import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 /*
  *  Copyright (c) 2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
@@ -21,33 +15,9 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
  */
 class SetupFragment : Fragment(R.layout.fragment_setup) {
 
-    private val appStatusViewModel: AppConfigViewModel by sharedViewModel()
-    private val mobileCoreWrapper: MobileCoreWrapper by inject()
-    private val setupViewModel: SetupViewModel by inject()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         Accessibility.announce(requireContext(), getString(R.string.app_setup_text))
-
-        setObservers()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        appStatusViewModel.refresh(mobileCoreWrapper, true)
-    }
-
-    private fun setObservers() {
-        appStatusViewModel.appStatusLiveData.observe(viewLifecycleOwner) {
-            if (it is AppStatus.NoActionRequired || it is AppStatus.UpdateRecommended) {
-                setupViewModel.onConfigUpdated()
-            }
-        }
-        setupViewModel.introductionDataLiveData.observe(viewLifecycleOwner) {
-            findNavControllerSafety()?.navigate(
-                SetupFragmentDirections.actionOnboarding(it)
-            )
-        }
     }
 }
